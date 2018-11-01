f86923823d1e61871e7a33231726751ff08000d6
ineol <leo.lveb@gmail.com>
2018-11-02 00:06:29 +0100

    Almost the definition of the logical relation

--- theories/unary_lr.v
+++ theories/unary_lr.v
@@ -19,43 +19,10 @@ Section Sec.
   Definition SP γ ϕ := saved_pred_own γ ϕ.
   Notation "γ ⤇ ϕ" := (SP γ ϕ) (at level 200).
 
-  (* Check SP. *)
-  (* Check (SP: gname -> DotPred -> iProp Σ). *)
-  (* Notation "'DotPred'" := (vr -> iProp Σ) (at level 100). *)
-  (* Definition vr_prop := vr -> iProp Σ. *)
-  (* Arguments vr_prop /. *)
-  (* Print solve_decision. *)
-  (* Instance id_dec_eq (i i' : id) : Decision (i = i'). *)
-  (* Proof. solve_decision. Defined. *)
-  (* Instance gname_dec_eq (i i' : gname) : Decision (i = i'). *)
-  (* Proof. solve_decision. Defined. *)
-  
-  (* Fixpoint vr_dec_eq (v v' : vr) : Decision (v = v') *)
-  (*   with ty_dec_eq (t t' : ty) : Decision (t = t') *)
-  (*   with tm_dec_eq (t t' : tm) : Decision (t = t') *)
-  (*   with dm_dec_eq (t t' : dm) : Decision (t = t') *)
-  (*   with dms_dec_eq (t t' : dms) : Decision (t = t'). *)
-  (* Proof. *)
-  (*   all: unfold Decision in *. *)
-  (*   all: decide equality; try apply id_dec_eq; try apply gname_dec_eq. *)
-  (* Defined. *)
-  (* (*   decide equality; try typeclasses eauto. try apply id_dec_eq. *) *)
-  (* (*   decide equality. *) *)
-  (* (* Print solve_decision. *) *)
-  (* (*   solve *) *)
-  (* (*   all: solve_decision. Defined. *) *)
-
-
-  (* Canonical Structure vrC3 := discreteC vr. *)
   Canonical Structure vlC := leibnizC vl.
   Canonical Structure tmC := leibnizC tm.
-  (* Canonical Structure vrC := discreteC vr. *)
   Notation D := (vlC -n> iProp Σ).
   Implicit Types τi : D.
-
-  (* Print list. *)
-  (* SearchAbout (list ?X -> nat -> option ?X). *)
-  (* Check lookup. *)
 
   Fixpoint dms_to_list (ds: dms) : list dm :=
     match ds with
@@ -68,14 +35,59 @@ Section Sec.
 
   Program Definition expr_sem (A: D) (e: tm) : iProp Σ :=
     (False % I).
-  Fixpoint interp (T: ty) : D := λne v,
-    match T with
-    | TAnd T1 T2 => interp T1 v ∧ interp T2 v
-    | TOr T1 T2 => interp T1 v ∨ interp T2 v
-    | TTMem l L U => False
-    | _ => False
-    end % I.
+
+  Program Definition interp_and (interp1 interp2 : listC vlC -n> D) : listC vlC -n> D := λne ρ v,
+    (interp1 ρ v  ∧  interp2 ρ v) % I.
+  Solve Obligations with solve_proper.
+
+
+  Program Definition interp_or (interp1 interp2 : listC vlC -n> D) : listC vlC -n> D := λne ρ v,
+    (interp1 ρ v  ∨  interp2 ρ v) % I.
+  Solve Obligations with solve_proper.
+
+  Notation inclusion P Q := (∀ v, P v -∗ Q v)%I.
+  (* Definition inclusion1 (P : D) (Q : vl -> iProp Σ) : iProp Σ := (∀ v, P v -∗ Q v)%I. *)
+  (* Definition inclusion1 (Q : vl -> iProp Σ) : iProp Σ := (∀ v, P v -∗ Q v)%I. *)
+
+  Program Definition interp_mem (l: label) (interp1 interp2 : listC vlC -n> D) : listC vlC -n> D := λne ρ v,
+    (∃ γ ϕ ds, ⌜ v = vobj ds ∧ index_dms l ds = Some(dtysem γ) ⌝ ∧ (SP γ ϕ) ∗ (inclusion (interp1 ρ) ϕ) ∗ inclusion ϕ (interp2 ρ) ) % I.
+  Solve Obligations with solve_proper.
+
+  Program Definition interp_later (interp : listC vlC -n> D) : listC vlC -n> D := λne ρ v,
+         (▷ (interp ρ v)) % I.
+  Solve Obligations with solve_proper.
+
+  Program Definition interp_forall (interp1 interp2 : listC vlC -n> D) : listC vlC -n> D := λne ρ v,
+    (▷ ∀ v', interp1 ρ v' -∗ expr_sem (interp2 (v :: ρ)) (tapp (tv v) (tv v'))) % I.
+  Solve Obligations with solve_proper.
+
+  Program Definition interp_val_mem l (interp : listC vlC -n> D) : listC vlC -n> D := λne ρ v,
+    (∃ ds vmem, ⌜ v = vobj ds ∧ index_dms l ds = Some(dvl vmem) ⌝ ∧ ▷ interp ρ vmem) % I.
+  Solve Obligations with solve_proper.
+
+  Program Definition interp_bind (interp : listC vlC -n> D) : listC vlC -n> D := λne ρ v,
+    (interp (v::ρ) v) % I.
+  Solve Obligations with solve_proper.
   
+  Program Definition interp_true : listC vlC -n> D := λne ρ v, True % I.
+  Program Definition interp_false : listC vlC -n> D := λne ρ v, False % I.
+
+  Fixpoint interp (T: ty) : listC vlC -n> D := 
+    match T with
+    | TAnd T1 T2 => interp_and (interp T1) (interp T2)
+    | TOr T1 T2 => interp_or (interp T1) (interp T2)
+    | TTMem l L U => interp_mem l (interp L) (interp U)
+    | TLater T => interp_later (interp T)
+    | TTop => interp_true
+    | TBot => interp_false
+    | TAll T1 T2 => interp_forall (interp T1) (interp T2)
+    | TBind T => interp_bind (interp T)
+    | TSel value (* l *) => interp_true (* ∃ γ ϕ ds, ⌜ v = vobj ds ∧ index_dms l ds = Some(dtysem γ) ⌝ ∧ (SP γ ϕ) *)
+    | TSelA _ _ _ => interp_false
+    | TVMem l T' => interp_val_mem l (interp T')
+  end % I.
+
+
   (* V􏰃p.A􏰄ρ= 􏰋v 􏰍 ∃φ.ρ(p).A↘φ∧▷(v∈φ)􏰌 *)
   
   (* Set Implicit Arguments. *)
@@ -88,14 +100,14 @@ Section Sec.
   (*   (□ (∃ v, ⌜w = FoldV v⌝ ∧ ▷ interp (τi :: Δ) v))%I. *)
 
   (* Global Instance interp_rec1_contractive *)
-  (*   (interp : listC D -n> D) (Δ : listC D) : Contractive (interp_rec1 interp Δ). *)
+  (*   (interp : listC vlC -n> D) (Δ : listC vlC) : Contractive (interp_rec1 interp Δ). *)
   (* Proof. by solve_contractive. Qed. *)
 
-  (* Lemma fixpoint_interp_rec1_eq (interp : listC D -n> D) Δ x : *)
+  (* Lemma fixpoint_interp_rec1_eq (interp : listC vlC -n> D) Δ x : *)
   (*   fixpoint (interp_rec1 interp Δ) x ≡ interp_rec1 interp Δ (fixpoint (interp_rec1 interp Δ)) x. *)
   (* Proof. exact: (fixpoint_unfold (interp_rec1 interp Δ) x). Qed. *)
 
-  (* Program Definition interp_rec (interp : listC D -n> D) : listC D -n> D := λne Δ, *)
+  (* Program Definition interp_rec (interp : listC vlC -n> D) : listC vlC -n> D := λne Δ, *)
   (*   fixpoint (interp_rec1 interp Δ). *)
   (* Next Obligation. *)
   (*   intros interp n Δ1 Δ2 HΔ; apply fixpoint_ne => τi w. solve_proper. *)
