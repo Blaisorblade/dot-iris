05b3110bfdf8851bd8e9403fcd3732bb5a670278
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-11-02 15:15:11 +0100

    Prove a couple of typing lemmas as Iris exercise

--- theories/unary_lr.v
+++ theories/unary_lr.v
@@ -109,4 +109,39 @@ Section Sec.
                )%I
     end.
   Notation "⟦ Γ ⟧*" := (interp_env Γ).
+
+  (* Some subtyping lemmas, directly in terms of interp. *)
+  Lemma stp_later T ρ v (H: interp T ρ v): interp (TLater T) ρ v.
+  Proof. simpl; iNext. iApply H. Qed.
+
+  Lemma stp_and1 T1 T2 ρ v: interp (TAnd T1 T2) ρ v -> interp T1 ρ v.
+  Proof.
+    simpl in *. intros Hv.
+    iDestruct Hv as "[H1 H2]".
+    iAssumption.
+  Qed.
+
+  Lemma stp_and2 T1 T2 ρ v: interp (TAnd T1 T2) ρ v -> interp T2 ρ v.
+  Proof.
+    simpl in *. intros Hv.
+    iDestruct Hv as "[H1 H2]".
+    iAssumption.
+  Qed.
+
+  Lemma stp_and_w T1 T2 ρ v:
+    interp T1 ρ v ->
+    interp T2 ρ v ->
+    interp (TAnd T1 T2) ρ v.
+  Proof.
+    simpl in *. intros.
+    iSplit.
+    iApply H.
+    iApply H0.
+  Qed.
+
+  Lemma stp_and T1 T2 ρ v:
+    interp T1 ρ v -∗
+    interp T2 ρ v -∗
+    interp (TAnd T1 T2) ρ v.
+  Proof. iIntros; by iSplit. Qed.
 End Sec.
