ae4764651057d2d94d359a2dd00418fe7d330b89
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-11-04 12:14:55 +0100

    "Prove" (up to binding) rules on mu

--- theories/lr_lemma.v
+++ theories/lr_lemma.v
@@ -113,4 +113,89 @@ Section Sec.
     iNext; by iRewrite "Hag".
   Qed.
 
+  (*
+     Γ, z: T₁ᶻ ⊨ T₁ᶻ <: T₂ᶻ
+     ----------------------------------------------- (<:-μ-X)
+     Γ ⊨ μ (x: T₁ˣ) <: μ(x: T₂ˣ)
+  *)
+  Lemma ivstp_mu_x T1 T2:
+    ivstp (T1 :: Γ) T1 T2 -∗
+    ivstp Γ (TMu T1) (TMu T2).
+  Proof.
+    iIntros "/= #Hstp !>" (v ρ) "#Hg #HT1".
+    iApply "Hstp"; by try iSplit.
+  Qed.
+
+  Implicit Types T: ty.
+
+  (*
+     Γ, z: T₁ᶻ ⊨ T₁ᶻ <: T₂
+     ----------------------------------------------- (<:-Bind-1)
+     Γ ⊨ μ (x: T₁ˣ) <: T₂
+  *)
+  Lemma ivstp_mu_1 T1 T2:
+    ivstp (T1 :: Γ) T1 (T2.[up_ty_vl var_vl]) -∗
+    ivstp Γ (TMu T1) T2.
+  Proof.
+    iIntros "/= #Hstp !>" (v ρ) "#Hg #HT1".
+    (* Hopefully from a renaming/weakening lemma. *)
+    iAssert (interp T2 ρ v ≡ interp T2.[up_ty_vl var_vl] (v :: ρ) v)%I as "#Hren".
+    { admit. }
+    iRewrite "Hren".
+    iApply "Hstp"; by try iSplit.
+  Admitted.
+
+  (*
+     Γ, z: T₁ᶻ ⊨ T₁ <: T₂ᶻ
+     ----------------------------------------------- (<:-Bind-2)
+     Γ ⊨ T₁ <: μ(x: T₂ˣ)
+  *)
+  Lemma ivstp_mu_2 T1 T2:
+    ivstp (T1.[up_ty_vl var_vl] :: Γ) (T1.[up_ty_vl var_vl]) T2 -∗
+    ivstp Γ T1 (TMu T2).
+  Proof.
+    iIntros "/= #Hstp !>" (v ρ) "#Hg #HT1".
+    (* Hopefully from a renaming/weakening lemma. *)
+    iAssert (interp T1 ρ v ≡ interp T1.[up_ty_vl var_vl] (v :: ρ) v)%I as "#Hren".
+    { admit. }
+    iRewrite "Hren" in "HT1".
+    iApply "Hstp"; by try iSplit.
+  Admitted.
+
+  (* BEWARE NONSENSE IN NOTES:
+     Γ ⊨ x: Tˣ
+     ----------------------------------------------- (<:)
+     Γ ⊨ mu(x: Tˣ) <: Tˣ    Γ ⊨ Tˣ <: mu(x: Tˣ)
+
+     Luckily we don't need that, all the rules that exist before appear reasonable. *)
+
+  (*
+     Γ ⊨ z: Tᶻ
+     =============================================== (T-Rec-I/T-Rec-E)
+     Γ ⊨ z: mu(x: Tˣ)
+   *)
+  Lemma ivstp_rec_eq T v:
+    ivtp Γ (TMu T) v ≡
+    ivtp Γ T.[v/] v.
+  Proof.
+    iAssert (□ ∀ ρ, interp_env Γ ρ -∗ interp T.[v/] ρ v ≡ interp T (v :: ρ) v)%I as "#Hren".
+    { admit. }
+    iSplit; iIntros "/= #Htp !>" (ρ) "#Hg";
+      iSpecialize ("Htp" $! ρ); iSpecialize ("Hren" $! ρ with "Hg").
+    - iRewrite "Hren".
+      by iApply "Htp".
+    - iRewrite "Hren" in "Htp".
+      by iApply "Htp".
+  Admitted.
+
+  Lemma ivstp_rec_i T v:
+    ivtp Γ T.[v/] v -∗
+    ivtp Γ (TMu T) v.
+  Proof. by iDestruct ivstp_rec_eq as "[? ?]". Qed.
+
+  Lemma ivstp_rec_e T v:
+    ivtp Γ (TMu T) v -∗
+    ivtp Γ T.[v/] v.
+  Proof. by iDestruct ivstp_rec_eq as "[? ?]". Qed.
+
 End Sec.
