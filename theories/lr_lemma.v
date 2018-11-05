3a94504a62e2f2f68a83f8cdf31b8fc7467d3c17
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-11-05 01:39:07 +0100

    Start adding lemmas on definition typing

--- theories/lr_lemma.v
+++ theories/lr_lemma.v
@@ -210,4 +210,93 @@ Section Sec.
     ivtp Γ T.[v/] v.
   Proof. by iDestruct ivstp_rec_eq as "[? ?]". Qed.
 
+  (**
+     Lemmas about definition typing.
+     TODO: generalize them for definitions at arbitrary positions.
+
+     Yak to shave for that TODO: it's probably better to make labels index
+     definitions from the end, as done by existing formalizations. This way,
+     [dcons d ds] keeps the existing labels for [ds] and uses a new one ([length
+     ds]) for [d]. That's a bit like de Bruijn levels.
+   *)
+  Definition idtp Γ T ds : iProp Σ := (□∀ ρ, ⟦Γ⟧* ρ -∗ defs_interp T ρ ds)%I.
+  Arguments idtp /.
+
+  Bind Scope dms_scope with dms.
+  Open Scope dms_scope.
+  Notation " [@ ] " := dnil (format "[@ ]") : dms_scope.
+  Notation " [@ x ] " := (dcons x dnil) : dms_scope.
+  Notation " [@ x ; y ; .. ; z ] " := (dcons x (dcons y .. (dcons z dnil) ..))
+                                      : dms_scope.
+  Lemma idtp_vmem_i T v:
+    ▷ ivtp Γ T v -∗
+      idtp Γ (TVMem 0 T) [@ dvl v].
+  Proof.
+    iIntros "#Hv !> * #Hg /=".
+    iExists v; iSplit.
+    - repeat iPureIntro; done.
+    - by iApply "Hv".
+  Qed.
+
+  (*
+    What I'd want, if we store envD instead, is closer to:
+
+    Lemma idtp_tmem_i T γ:
+      SP γ ⟦T⟧ -∗
+      idtp Γ (TTMem 0 T T) [@ dtysem γ].
+    Tho we need something about syntactic definitions as well.
+  *)
+  Lemma dtp_tmem_i T γ ρ:
+    SP γ (⟦T⟧ ρ) -∗ ⟦Γ⟧* ρ -∗
+    defs_interp (TTMem 0 T T) ρ [@ dtysem γ].
+  Proof.
+    iIntros "#Hv * #Hg /=".
+    iExists (⟦T⟧ ρ); iSplit.
+    iExists γ; by iSplit.
+    iSplit; by iIntros "!> **".
+  Qed.
+
+  (* We can't write idtp_tmem_i as above, but for now we can write: *)
+  (* Lemma idtp_tmem_i T γ ρ: *)
+  (*   SP γ (⟦T⟧ ρ) -∗ *)
+  (*      idtp Γ (TTMem 0 T T) [@ dtysem γ]. *)
+  (* Proof. *)
+  (*   iIntros "#Hv !> * #Hg /=". *)
+  (*   iExists (⟦T⟧ ρ); iSplit. *)
+  (*   iExists γ; by iSplit. *)
+  (*   iSplit; iIntros "!> **". *)
+  (* Aborted. *)
+
+
+  Lemma dtp_tmem_abs_i T L U γ ρ:
+    SP γ (⟦T⟧ ρ) -∗ ⟦Γ⟧* ρ -∗
+    (* I'd want to require these two hypotheses to hold later. *)
+    Γ ⊨ T <: U -∗
+    Γ ⊨ L <: T -∗
+    defs_interp (TTMem 0 L U) ρ [@ dtysem γ].
+  Proof.
+    iIntros "#Hv * #Hg #HTU #HLT /=".
+    iExists (⟦T⟧ ρ); iSplit.
+    iExists γ; by iSplit.
+    iSplit; iIntros "!> **"; [iApply "HLT" | iApply "HTU"]; done.
+  Qed.
+
+  Lemma alloc_sp T ρ:
+    (|==> ∃ γ, SP γ (⟦T⟧ ρ))%I.
+  Proof. by apply saved_pred_alloc. Qed.
+
+  Lemma alloc_dtp_tmem_i T ρ:
+    ⟦Γ⟧* ρ -∗
+    (|==> ∃ γ, defs_interp (TTMem 0 T T) ρ [@ dtysem γ])%I.
+  Proof.
+    iIntros "#Hg /=".
+    iDestruct (alloc_sp T ρ) as "HupdSp".
+    iMod "HupdSp" as (γ) "#Hsp".
+    iModIntro.
+    iExists γ, (⟦T⟧ρ).
+    iSplit.
+    - iExists γ; by iSplit.
+    - iSplit; by iIntros "!> **".
+  Qed.
+
 End Sec.
