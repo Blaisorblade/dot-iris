554146aef999866f3048f0cd62af14555c0f992a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-12-02 00:08:05 +0100

    Make lr_lemmasTSel.v compile again

--- theories/lr_lemmasTSel.v
+++ theories/lr_lemmasTSel.v
@@ -34,22 +34,26 @@ Section Sec.
     iIntros "/= !> * Hg #HvL".
     iDestruct ("Hva" $! ρ with "Hg") as (d) "#[% #H]"; iClear "Hva".
     iDestruct "H" as (φ σ) "#[Hl [#HLφ [#HφU #HLU]]]".
+    repeat iSplit; first by iApply "HLU".
+    iRight.
     iExists σ, φ, d.
     iDestruct ("HLφ" with "HvL") as "#HLφ'".
     iDestruct ("HLU" with "HvL") as "#HLU'".
-    repeat iModIntro; repeat iSplit; trivial. by iRight.
+    repeat iModIntro; by repeat iSplit.
   Qed.
 
   Lemma mem_stp_sel_sub L U va l:
     ivtp Γ (TTMem l L U) va -∗
-    uvstp Γ (TLater L) (TSel (pv va) l).
+    ivstp Γ (TLater L) (TSel (pv va) l).
   Proof.
     iIntros "/= #Hva !> * #Hg #HvL".
     iDestruct ("Hva" $! ρ with "Hg") as (d) "#[% #H]"; iClear "Hva".
     iDestruct "H" as (φ σ) "#[Hl [#HLφ [#HφU #HLU]]]".
+    repeat iSplit; first done.
+    iRight.
     iExists σ , φ, d.
     iDestruct ("HLφ" with "HvL") as "#HLφ'".
-    repeat iModIntro; repeat iSplit; trivial. by iRight.
+    repeat iModIntro; by repeat iSplit.
   Qed.
 
   Instance Inh_vl: Inhabited vl.
@@ -67,7 +71,7 @@ Section Sec.
   (* Next step: proper lemma on arbitrary-length paths. *)
   Lemma mem_stp_sel_sub_path1 L U va l1 l2:
     (ivtp Γ (TVMem l1 (TTMem l2 L U)) va -∗
-    uvstp Γ (TLater L) ((TSel (pself (pv va) l1) l2)))%I.
+    ivstp Γ (TLater L) ((TSel (pself (pv va) l1) l2)))%I.
   Proof.
     iIntros "/= #Hva !> * #Hg #HvL".
 
@@ -76,11 +80,12 @@ Section Sec.
     iDestruct "H1" as (d) "[Hl2 H]".
     iDestruct "H" as (φ σ) "#[Hl [#HLφ [#HφU #HLU]]]".
     iDestruct ("HLφ" with "HvL") as "HLφ'".
+    repeat iSplit; first done.
+    iRight.
     iExists vmem.
-    repeat iModIntro; repeat iSplit; try done.
+    repeat iSplit; try done.
     iExists σ , φ, d.
-    repeat iModIntro; repeat iSplit; try done.
-    by iRight.
+    repeat iModIntro; by repeat iSplit.
   Qed.
 
 
@@ -97,30 +102,30 @@ Section Sec.
 
   Lemma mem_stp_sub_sel L U va l:
     ivtp Γ (TTMem l L U) va -∗
-    uvstp Γ (TSel (pv va) l) (TLater U).
+    ivstp Γ (TSel (pv va) l) (TLater U).
   Proof.
-    iIntros "/= #Hva !> * #Hg #Hφ".
+    iIntros "/= #Hva !> * #Hg #[_ [[] | Hφ]]".
     iDestruct ("Hva" $! ρ with "Hg") as (d) "#[% #H]"; iClear "Hva".
     iDestruct "H" as (φ σ) "#[Hlφ [HLφ [HφU #HLU]]]".
     iDestruct "Hlφ" as (γ) "[% Hγφ]".
 
     iApply "HφU".
-    iDestruct "Hφ" as (σ1 φ1 d1 Hva) "[Hγ [_ [False | #HΦ1v]]]"; try done.
+    iDestruct "Hφ" as (σ1 φ1 d1 Hva) "[Hγ #HΦ1v]".
+    (* " [_ [False | #HΦ1v]]]"; try done. *)
     iDestruct "Hγ" as (γ' Hd1) "HγΦ1".
 
-    injectHyps; objLookupDet; subst.
+    injectHyps; subst; objLookupDet.
 
     iAssert (∀ ρ v, ▷ (φ ρ v ≡ φ1 ρ v))%I as "#Hag".
     { iIntros.
-      (* fails T_T : iApply (saved_interp_agree_eta γ φ φ1 ρ0 v0). *)
-      admit.
+      (* Paolo: This manual eta-expansion is needed to get coercions to apply. *)
+      by iApply (saved_interp_agree_eta γ (λ a, φ a) (λ a, φ1 a) ρ0 v0).
     }
 
     (* iAssert (▷ (subst_phi σ ρ φ v ≡ subst_phi σ ρ φ1 v))%I as "#Hag"; simpl. *)
     (*  ▷ (subst_phi σ ρ φ v ≡ subst_phi σ ρ φ1 v))%I as "#Hag". simpl. *)
     (* { qy iApply saved_pred_agree. } *)
     repeat iModIntro.
-    inversion H1; subst.
     by iRewrite ("Hag" $! σ v).
-  Admitted.
+  Qed.
 End Sec.
