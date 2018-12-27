b63cae08f354284e06d76d058a53c46d092a78f3
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-12-28 00:48:21 +0100

    Cleanup: equality is enough here

--- theories/synLemmas.v
+++ theories/synLemmas.v
@@ -1,9 +1,9 @@
 (**
 Lemmas on SynFuncs.v needed for proofs on the operational semantics.
 To reduce compile times, unary_lr should not depend on this file.
-This file should load as little Iris code as possible, to reduce compile times. But we must import operational.
+This file should load as little Iris code as possible, to reduce compile times.
  *)
-From Dot Require Import tactics dotsyn operational.
+From Dot Require Import tactics dotsyn synFuncs.
 
 (** Rewrite v ↗ ds to vobj ds' ↗ ds. *)
 Ltac simplOpen ds' :=
@@ -124,7 +124,7 @@ Lemma length_idsσ n: length (idsσ n) = n.
 Proof. pose proof (length_idsσr n (+0)) as Hr. asimpl in Hr. exact Hr. Qed.
 
 Lemma subst_sigma_idsσ ρ n : length ρ = n →
-                (subst_sigma (idsσ n) ρ) ≡ ρ.
+                (subst_sigma (idsσ n) ρ) = ρ.
 Proof.
   rewrite /subst_sigma. move :n.
   induction ρ => *; subst => //; asimpl.
