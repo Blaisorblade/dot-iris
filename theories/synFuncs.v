939b82b19e4e71c42a294d6cb6698206b9f91de0
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-12-28 00:55:47 +0100

    Drop dead code

--- theories/synFuncs.v
+++ theories/synFuncs.v
@@ -9,38 +9,8 @@ From iris.base_logic.lib Require Import invariants.
 
 Implicit Types (L T U: ty) (v: vl) (e: tm) (d: dm) (Γ : list ty).
 
-(* Fixpoint dms_to_list (ds: dms) : list dm := *)
-(*   match ds with *)
-(*   | dnil => [] *)
-(*   | dcons d ds => d :: dms_to_list ds *)
-(*   end. *)
-
-(* Fixpoint vls_to_list (vs: vls) : list vl := *)
-(*   match vs with *)
-(*   | vlnil => [] *)
-(*   | vlcons v vs => v :: vls_to_list vs *)
-(*   end. *)
-
-(* (** Reverse lookup. *) *)
-(* Fixpoint indexr {X} (i: nat) (xs: list X) : option X := *)
-(*   match xs with *)
-(*   | [] => None *)
-(*   | x :: xs => *)
-(*     if decide (i = length xs) then Some x else indexr i xs *)
-(*   end. *)
-
-(* Definition index_dms (i: label) (ds: dms): option dm := *)
-(*   indexr i (dms_to_list ds). *)
-
-(* Definition dms_length ds := length (dms_to_list ds). *)
-
-(* (** Single-variable substitution, based on the Autosubst1 notation. Priorities copied from s .[ sigma ]. *) *)
-(* Notation "s .[ v /]" := (s .[ v .: var_vl ]) *)
-(*   (at level 2, v at level 200, left associativity, *)
-(*    format "s .[ v /]" ) : subst_scope. *)
-
-(* (** Substitute object inside itself (to give semantics to the "self" *)
-(*     variable). To use when descending under the [vobj] binder. *) *)
+(** Substitute object inside itself (to give semantics to the "self" *)
+(*     variable). To use when descending under the [vobj] binder. *)
 Definition selfSubst (ds: dms): dms := ds.|[vobj ds/].
 
 Definition objLookup v l d: Prop :=
