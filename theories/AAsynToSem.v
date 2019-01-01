9d816fa0e3cfff863fc281703c8a9c00abbf64c2
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-01-01 14:49:35 +0100

    Move out definition about skeleton sharing

--- theories/AAsynToSem.v
+++ theories/AAsynToSem.v
@@ -1,7 +1,7 @@
 (** Define translation from syntactic terms/values to semantic ones, following Sec. 3.2 of the PDF. *)
 From iris.proofmode Require Import tactics.
 From iris.program_logic Require Import ectx_lifting.
-From Dot Require Import tactics operational synLemmas.
+From Dot Require Import tactics operational skeleton synLemmas.
 
 Section Sec.
   Context `{HdotG: dotG Σ}.
@@ -362,71 +362,6 @@ Section Sec.
 
   Admitted.
 
-  Fixpoint same_skel_tm (t1 t2: tm): Prop :=
-    match (t1, t2) with
-    | (tv v1, tv v2) => same_skel_vl v1 v2
-    | (tapp t11 t12, tapp t21 t22) =>
-      same_skel_tm t11 t21 ∧ same_skel_tm t12 t22
-    | (tproj t1 l1, tproj t2 l2) =>
-      same_skel_tm t1 t2 ∧ l1 = l2
-    | (tskip t1, tskip t2) =>
-      same_skel_tm t1 t2
-    | _ => False
-    end
-  with
-  same_skel_vl (v1 v2: vl): Prop :=
-    match (v1, v2) with
-    | (var_vl i1, var_vl i2) => i1 = i2
-    | (vabs t1, vabs t2) => same_skel_tm t1 t2
-    | (vobj ds1, vobj ds2) =>
-      let fix same_skel_dms (ds1 ds2: dms): Prop :=
-          match (ds1, ds2) with
-          | (nil, nil) => True
-          | (cons d1 ds1, cons d2 ds2) =>
-            same_skel_dm d1 d2 ∧ same_skel_dms ds1 ds2
-          | _ => False
-          end
-      in same_skel_dms ds1 ds2
-    | (vnat n1, vnat n2) => n1 = n2
-    | _ => False
-    end
-  with
-  same_skel_dm (d1 d2: dm): Prop :=
-    match (d1, d2) with
-    | (dtysyn T1, dtysem σ2 γ2) =>
-      (* Only nontrivial case *)
-      True
-    | (dvl v1, dvl v2) => same_skel_vl v1 v2
-    | _ => False
-    end.
-  Fixpoint same_skel_path (p1 p2: path): Prop :=
-    match (p1, p2) with
-    | (pv v1, pv v2) => same_skel_vl v1 v2
-    | (pself p1 l1, pself p2 l2) => same_skel_path p1 p2 ∧ l1 = l2
-    | _ => False
-    end.
-  Fixpoint same_skel_ty (T1 T2: ty): Prop :=
-    match (T1, T2) with
-    | (TTop, TTop) => True
-    | (TBot, TBot) => True
-    | (TAnd T11 T12, TAnd T21 T22) =>
-      same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-    | (TOr T11 T12, TOr T21 T22) =>
-      same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-    | (TLater T1, TLater T2) =>
-      same_skel_ty T1 T2
-    | (TAll T11 T12, TAll T21 T22) =>
-      same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-    | (TMu T1, TMu T2) =>
-      same_skel_ty T1 T2
-    | (TVMem l1 T1, TVMem l2 T2) => l1 = l2 ∧ same_skel_ty T1 T2
-    | (TTMem l1 T11 T12, TTMem l2 T21 T22) => l1 = l2 ∧ same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-    | (TSel p1 l1, TSel p2 l2) => same_skel_path p1 p2 ∧ l1 = l2
-    | (TSelA p1 l1 T11 T12, TSelA p2 l2 T21 T22) => same_skel_path p1 p2 ∧ l1 = l2 ∧ same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-    | (TNat, TNat) => True
-    | _ => False
-    end.
-
   Lemma translation_same_skel t t':
     ( t_tm t t' → ⌜same_skel_tm t t'⌝)%I
   with translation_same_ty t t':
@@ -458,90 +393,6 @@ Section Sec.
     repeat iSplit;
       try (iDestruct "Hyp" as "[Hyp1 Hyp2]"); localAuto.
     by iApply "IHl".
-  Qed.
-
-  Fixpoint same_skel_ectx K K' :=
-    match K, K' with
-    | AppLCtx e2, AppLCtx e2' => same_skel_tm e2 e2'
-    | AppRCtx v1, AppRCtx v1' => same_skel_vl v1 v1'
-    | ProjCtx l, ProjCtx l' => l = l'
-    | _, _ => False
-    end.
-
-  Definition same_skel_list_ectx Ks Ks' :=
-    List.Forall2 same_skel_ectx Ks Ks'.
-
-  Lemma same_skel_fill Ks e t':
-    same_skel_tm (fill Ks e) t' →
-    exists Ks' e',  t' = fill Ks' e' ∧ same_skel_tm e e' ∧ same_skel_list_ectx Ks Ks'.
-  Proof. Admitted.
-
-  Lemma same_skel_fill_item Ks Ks' e e':
-    same_skel_list_ectx Ks Ks' →
-    same_skel_tm e e' →
-    same_skel_tm (fill Ks e) (fill Ks' e').
-  Admitted.
-
-  (* Generalize over all the syntax, and same_skeleton environments. I proved
-     same_skel_tm_subst just as a minimal sanity check. *)
-  Lemma same_skel_vl_subst e e' v v':
-    same_skel_vl e e' → same_skel_vl v v' →
-    same_skel_vl (e.[v/]) (e'.[v'/]).
-  Proof.
-    move: e'; induction e; destruct e';
-    move => Hske Hskv;
-      cbn in Hske |- *; try inversion Hske; ev; asimpl; auto.
-  Admitted.
-
-  (* Just a test proof. *)
-  Lemma same_skel_tm_subst e e' v v':
-    same_skel_tm e e' → same_skel_vl v v' →
-    same_skel_tm (e.|[v/]) (e'.|[v'/]).
-  Proof.
-    move: e'; induction e; destruct e';
-    move => Hske Hskv;
-      cbn in Hske |- *; try inversion Hske; ev; asimpl; auto using same_skel_vl_subst.
-   Qed.
-
-  Definition same_skel_dms (ds1 ds2: dms): Prop := Forall2 same_skel_dm ds1 ds2.
-
-  Lemma same_skel_dms_index ds ds' v l:
-    same_skel_dms ds ds' →
-    reverse (selfSubst ds) !! l = Some (dvl v) →
-    exists v', reverse (selfSubst ds') !! l = Some (dvl v') ∧ same_skel_vl v v'.
-  Proof.
-  Admitted.
-
-  Ltac destruct_matches :=
-    repeat progress match goal with
-                    | H: match ?t with | _ => _ end |- _ => destruct t; try done
-                    | H: ?A ∧ ?B |- _ => destruct H
-                    end.
-
-  Lemma simulation_skeleton_head t1' t1 t2 σ σ' ts:
-    same_skel_tm t1 t1' →
-    head_step t1 σ [] t2 σ' ts →
-    exists t2', head_step t1' σ [] t2' σ' ts ∧ same_skel_tm t2 t2'.
-  Proof.
-    move=> Hsk Hhs. inversion Hhs; subst; simpl in *.
-    - destruct_matches. eexists. split; first by econstructor. by eapply same_skel_tm_subst.
-    - destruct_matches. subst. destruct (same_skel_dms_index ds l1 v l0) as [? [? ?]]; try done.
-      + clear Hhs H0. red; generalize dependent l1; induction ds; destruct l1; try constructor; ev; try apply IHds; done.
-      + eexists.
-      split; [by econstructor | done].
-    - destruct_matches. eexists. split; by [econstructor|idtac].
-  Qed.
-
-  Theorem simulation_skeleton t1 t1' t2 σ σ' ts:
-    same_skel_tm t1 t1' →
-    prim_step t1 σ [] t2 σ' ts →
-    exists t2', prim_step t1' σ [] t2' σ' ts ∧ same_skel_tm t2 t2'.
-  Proof.
-    move=> Hsk Hstep. rewrite /prim_step in Hstep. simpl in *.
-    inversion Hstep; subst; simpl in *.
-    apply same_skel_fill in Hsk as [Ks' [e' [Hfill [Hskel Hsklsit]]]].  simpl in *.
-    eapply simulation_skeleton_head in H2 as [e'' [Hhs Hskk]]; last by eauto.
-    exists (fill Ks' e''). subst. split; [econstructor; eauto | by apply same_skel_fill_item].
   Qed.
 
   (** We want to prove that different translations give equivalent lookup
