51c59ebd6da942e5e21127f065654613f34bfbe2
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-02-28 07:02:11 +0100

    Prove that typing implies well-stamping (#21)
    We take well-stamping of context as hypothesis (following PFPL 2nd ed.'s
    regularity, Lemma 16.1 for System F). We must also check well-stamping of all
    types we add to the typing context.

--- theories/Dot/typing.v
+++ theories/Dot/typing.v
@@ -41,11 +41,13 @@ Inductive typed Γ : tm → ty → Prop :=
     Γ ⊢ₜ tv v: T.|[v/] (** Introduction forms *)
 | Lam_typed e T1 T2:
     (* T1 :: Γ ⊢ₜ e : T2 → (* Would work, but allows the argument to occur in its own type. *) *)
+    is_stamped_ty (length Γ) getStampTable T1 →
     T1.|[ren (+1)] :: Γ ⊢ₜ e : T2 →
     (*─────────────────────────*)
     Γ ⊢ₜ tv (vabs e) : TAll T1 T2
 | VObj_typed ds T:
     Γ |ds T ⊢ ds: T →
+    is_stamped_ty (S (length Γ)) getStampTable T →
     (*──────────────────────*)
     Γ ⊢ₜ tv (vobj ds): TMu T
 | TMuI_typed v T:
@@ -113,35 +115,52 @@ with path_typed Γ : path → ty → nat → Prop :=
 where "Γ ⊢ₚ p : T , i" := (path_typed Γ p T i)
 (* Γ ⊢ₜ T1, i1 <: T2, i2 means that TLater^i1 T1 <: TLater^i2 T2. *)
 with subtype Γ : ty → nat → ty → nat → Prop :=
-| Refl_stp i T : Γ ⊢ₜ T, i <: T, i
+| Refl_stp i T :
+    is_stamped_ty (length Γ) getStampTable T →
+    Γ ⊢ₜ T, i <: T, i
 | Trans_stp i1 i2 i3 T1 T2 T3:
     Γ ⊢ₜ T1, i1 <: T2, i2 → Γ ⊢ₜ T2, i2 <: T3, i3 → Γ ⊢ₜ T1, i1 <: T3, i3
 | TLaterL_stp i T:
-   Γ ⊢ₜ TLater T, i <: T, S i
+    is_stamped_ty (length Γ) getStampTable T →
+    Γ ⊢ₜ TLater T, i <: T, S i
 | TLaterR_stp i T:
-   Γ ⊢ₜ T, S i <: TLater T, i
+    is_stamped_ty (length Γ) getStampTable T →
+    Γ ⊢ₜ T, S i <: TLater T, i
 
 (* "Structural" rules about indexes *)
 | TSucc_stp T i:
+    is_stamped_ty (length Γ) getStampTable T →
     Γ ⊢ₜ T, i <: T, S i
 | TMono_stp T1 T2 i:
     Γ ⊢ₜ T1, i <: T2, i →
     Γ ⊢ₜ T1, S i <: T2, S i
 
 (* "Logical" connectives *)
-| Top_stp i T : Γ ⊢ₜ T, i <: TTop, i
-| Bot_stp i T : Γ ⊢ₜ TBot, i <: T, i
+| Top_stp i T :
+    is_stamped_ty (length Γ) getStampTable T →
+    Γ ⊢ₜ T, i <: TTop, i
+| Bot_stp i T :
+    is_stamped_ty (length Γ) getStampTable T →
+    Γ ⊢ₜ TBot, i <: T, i
 | TAnd1_stp T1 T2 i:
+    is_stamped_ty (length Γ) getStampTable T1 →
+    is_stamped_ty (length Γ) getStampTable T2 →
     Γ ⊢ₜ TAnd T1 T2, i <: T1, i
 | TAnd2_stp T1 T2 i:
+    is_stamped_ty (length Γ) getStampTable T1 →
+    is_stamped_ty (length Γ) getStampTable T2 →
     Γ ⊢ₜ TAnd T1 T2, i <: T2, i
 | TAnd_stp T1 T2 U i:
     Γ ⊢ₜ U, i <: T1, i →
     Γ ⊢ₜ U, i <: T2, i →
     Γ ⊢ₜ U, i <: TAnd T1 T2, i
 | TOr1_stp T1 T2 i:
+    is_stamped_ty (length Γ) getStampTable T1 →
+    is_stamped_ty (length Γ) getStampTable T2 →
     Γ ⊢ₜ T1, i <: TOr T1 T2, i
 | TOr2_stp T1 T2 i:
+    is_stamped_ty (length Γ) getStampTable T1 →
+    is_stamped_ty (length Γ) getStampTable T2 →
     Γ ⊢ₜ T2, i <: TOr T1 T2, i
 | TOr_stp T1 T2 U i:
     Γ ⊢ₜ T1, i <: U, i →
@@ -165,12 +184,15 @@ with subtype Γ : ty → nat → ty → nat → Prop :=
 (* Subtyping for recursive types. Congruence, and opening in both directions. *)
 | mu_x_stp T1 T2 i:
     (T1 :: Γ) ⊢ₜ T1, i <: T2, i →
+    is_stamped_ty (S (length Γ)) getStampTable T1 →
     Γ ⊢ₜ TMu T1, i <: TMu T2, i
 | mu_1_stp T1 T2 i:
     (T1 :: Γ) ⊢ₜ T1, i <: T2.|[ren (+1)], i →
+    is_stamped_ty (S (length Γ)) getStampTable T1 →
     Γ ⊢ₜ TMu T1, i <: T2, i
 | mu_2_stp T1 T2 i:
     T1.|[ren (+1)] :: Γ ⊢ₜ T1.|[ren (+1)], i <: T2, i →
+    is_stamped_ty (length Γ) getStampTable T1 →
     Γ ⊢ₜ T1, i <: TMu T2, i
 
 (* "Congruence" or "variance" rules for subtyping. Unneeded for "logical" types.
@@ -188,6 +210,7 @@ with subtype Γ : ty → nat → ty → nat → Prop :=
     (* Non-tight premises. *)
     Γ ⊢ₜ T2, i <: T1, i →
     T2.|[ren (+1)] :: Γ ⊢ₜ U1, i <: U2, i →
+    is_stamped_ty (length Γ) getStampTable T2 →
     Γ ⊢ₜ TAll T1 U1, i <: TAll T2 U2, i
 | TVMemCov_stp T1 T2 i l:
     Γ ⊢ₜ T1, S i <: T2, S i →
@@ -198,28 +221,138 @@ with subtype Γ : ty → nat → ty → nat → Prop :=
     Γ ⊢ₜ TTMem l L1 U1, i <: TTMem l L2 U2, i
 where "Γ ⊢ₜ T1 , i1 <: T2 , i2" := (subtype Γ T1 i1 T2 i2).
 
-  Scheme typed_mut_ind := Induction for typed Sort Prop
-  with   dms_typed_mut_ind := Induction for dms_typed Sort Prop
-  with   dm_typed_mut_ind := Induction for dm_typed Sort Prop.
-  (* with   path_typed_mut_ind := Induction for path_typed Sort Prop
-  with   subtype_mut_ind := Induction for subtype Sort Prop. *)
+  Scheme exp_typed_mut_ind := Induction for typed Sort Prop
+  with   exp_dms_typed_mut_ind := Induction for dms_typed Sort Prop
+  with   exp_dm_typed_mut_ind := Induction for dm_typed Sort Prop
+  with   exp_path_typed_mut_ind := Induction for path_typed Sort Prop.
+  (* with   subtype_mut_ind := Induction for subtype Sort Prop. *)
 
-  Combined Scheme typing_mut_ind from typed_mut_ind, dms_typed_mut_ind, dm_typed_mut_ind.
+  Combined Scheme exp_typing_mut_ind from exp_typed_mut_ind, exp_dms_typed_mut_ind,
+    exp_dm_typed_mut_ind, exp_path_typed_mut_ind.
 
   Hint Constructors Forall.
-  Lemma stamped_mut Γ:
+  Lemma stamped_mut_subject Γ:
     (∀ e  T, Γ ⊢ₜ e : T → is_stamped_tm (length Γ) getStampTable e) ∧
     (∀ V ds T, Γ |ds V ⊢ ds : T → Forall (is_stamped_dm (S (length Γ)) getStampTable) (map snd ds)) ∧
-    (∀ V d T, Γ |d V ⊢ d : T → is_stamped_dm (S (length Γ)) getStampTable d).
+    (∀ V d T, Γ |d V ⊢ d : T → is_stamped_dm (S (length Γ)) getStampTable d) ∧
+    (∀ p T i, Γ ⊢ₚ p : T, i → is_stamped_path (length Γ) getStampTable p).
   Proof.
-    eapply typing_mut_ind with
+    eapply exp_typing_mut_ind with
         (P := λ Γ e T _, is_stamped_tm (length Γ) getStampTable e)
         (P0 := λ Γ V ds T _, Forall (is_stamped_dm (S (length Γ)) getStampTable) (map snd ds))
-        (P1 := λ Γ V d T _, is_stamped_dm (S (length Γ)) getStampTable d); cbn; eauto.
+        (P1 := λ Γ V d T _, is_stamped_dm (S (length Γ)) getStampTable d)
+        (P2 := λ Γ p T i _, is_stamped_path (length Γ) getStampTable p); cbn; intros; eauto.
     - repeat constructor.
     - intros; elim: i {s} => [|i IHi]; rewrite /= ?iterate_0 ?iterate_S //; eauto.
     - intros; ev. constructor; naive_solver.
     - intros; with_is_stamped inverse; eauto.
+  Qed.
+
+  Lemma stamped_exp_subject Γ e T: Γ ⊢ₜ e : T →
+    is_stamped_tm (length Γ) getStampTable e.
+  Proof. unmut_lemma (stamped_mut_subject Γ). Qed.
+  Lemma stamped_path_subject Γ p T i:
+    Γ ⊢ₚ p : T, i → is_stamped_path (length Γ) getStampTable p.
+  Proof. unmut_lemma (stamped_mut_subject Γ). Qed.
+
+  Scheme typed_mut_ind := Induction for typed Sort Prop
+  with   dms_typed_mut_ind := Induction for dms_typed Sort Prop
+  with   dm_typed_mut_ind := Induction for dm_typed Sort Prop
+  with   path_typed_mut_ind := Induction for path_typed Sort Prop
+  with   subtype_mut_ind := Induction for subtype Sort Prop.
+
+  Combined Scheme typing_mut_ind from typed_mut_ind, dms_typed_mut_ind, dm_typed_mut_ind,
+    path_typed_mut_ind, subtype_mut_ind.
+
+  Lemma stamped_ren n T g:
+    is_stamped_ty n g T <->
+    is_stamped_ty (S n) g (T.|[ren (+1)]).
+  Admitted.
+
+  Lemma stamped_subst_one n T v g:
+    is_stamped_ty (S n) g T →
+    is_stamped_vl n g v →
+    is_stamped_ty n g (T.|[v/]).
+  Admitted.
+
+  Lemma stamped_subst_one_rev n T v g:
+    is_stamped_ty n g (T.|[v/]) →
+    is_stamped_ty (S n) g T.
+  Admitted.
+
+  Hint Resolve stamped_path_subject stamped_ren stamped_subst_one stamped_subst_one_rev.
+
+  Inductive stamped_ctx g: ctx → Prop :=
+  | stamped_nil : stamped_ctx g []
+  | stamped_cons Γ T:
+    stamped_ctx g Γ →
+    is_stamped_ty (S (length Γ)) g T →
+    stamped_ctx g (T :: Γ)
+  .
+
+  Lemma stamped_lookup Γ x T g:
+    stamped_ctx g Γ →
+    Γ !! x = Some T →
+    is_stamped_ty (length Γ) g T.|[ren (+x)].
+  Proof.
+    elim: x Γ => /= [|x IHx] [|U Γ] /= Hctx Hl; asimpl; try discriminate.
+    - cinject Hl. by inverse Hctx.
+    - replace (T.|[ren (+S x)]) with (T.|[ren (+x)].|[ren (+1)]); last by asimpl.
+      eapply (stamped_ren (length Γ) (T.|[ren (+x)])), IHx; eauto. by inverse Hctx.
+  Qed.
+
+  (* n (Hctx: stamped_ctx getStampTable n Γ) *)
+  Lemma stamped_mut_types Γ :
+    (∀ e  T, Γ ⊢ₜ e : T → ∀ (Hctx: stamped_ctx getStampTable Γ), is_stamped_ty (length Γ) getStampTable T) ∧
+    (∀ V ds T, Γ |ds V ⊢ ds : T → ∀ (Hctx: stamped_ctx getStampTable Γ), is_stamped_ty (S (length Γ)) getStampTable V →
+      is_stamped_ty (S (length Γ)) getStampTable T) ∧
+    (∀ V d T, Γ |d V ⊢ d : T → ∀ (Hctx: stamped_ctx getStampTable Γ), is_stamped_ty (S (length Γ)) getStampTable V →
+      is_stamped_ty (S (length Γ)) getStampTable T) ∧
+    (∀ p T i, Γ ⊢ₚ p : T , i → ∀ (Hctx: stamped_ctx getStampTable Γ), is_stamped_ty (length Γ) getStampTable T) ∧
+    (∀ T1 i1 T2 i2, Γ ⊢ₜ T1, i1 <: T2, i2 → ∀ (Hctx: stamped_ctx getStampTable Γ),
+      is_stamped_ty (length Γ) getStampTable T1 ∧ is_stamped_ty (length Γ) getStampTable T2).
+  Proof.
+    eapply typing_mut_ind with
+        (P := λ Γ e T _, ∀ (Hctx: stamped_ctx getStampTable Γ),
+          is_stamped_ty (length Γ) getStampTable T)
+        (P0 := λ Γ V ds T _, ∀ (Hctx: stamped_ctx getStampTable Γ), is_stamped_ty (S (length Γ)) getStampTable V →
+          is_stamped_ty (S (length Γ)) getStampTable T)
+        (P1 := λ Γ V d T _, ∀ (Hctx: stamped_ctx getStampTable Γ), is_stamped_ty (S (length Γ)) getStampTable V →
+          is_stamped_ty (S (length Γ)) getStampTable T)
+        (P2 := λ Γ p T i _, ∀ (Hctx: stamped_ctx getStampTable Γ),
+          is_stamped_ty (length Γ) getStampTable T)
+        (P3 := λ Γ T1 i1 T2 i2 _, ∀ (Hctx: stamped_ctx getStampTable Γ),
+               is_stamped_ty (length Γ) getStampTable T1 ∧ is_stamped_ty (length Γ) getStampTable T2); clear Γ.
+    all: intros; cbn in *; ev; try solve [ eauto ].
+    all: try solve [try specialize (H Hctx); try specialize (H0 Hctx); ev;
+      with_is_stamped inverse; eauto; constructor; cbn; eauto].
+    (* Needed: substitution lemma for stamping. *)
+    - specialize (H Hctx). inverse H. cbn in *.
+      apply stamped_exp_subject in t0. inverse t0.
+      by eapply stamped_subst_one.
+    - specialize (H Hctx). inverse H. cbn in *. by apply stamped_ren.
+    - apply stamped_exp_subject in t. inverse t.
+      specialize (H Hctx). inverse H.
+      by eapply stamped_subst_one.
+    (* - constructor; eauto. apply stamped_ren. (* Needed: the context is well-stamped as well! *) admit. *)
+    - constructor; cbn; eauto. apply H.
+      econstructor; eauto. by eapply stamped_ren in f.
+    - by apply stamped_lookup.
+    - have Hctx': stamped_ctx getStampTable (TLater V :: Γ). by constructor => //; constructor.
+      specialize (H Hctx'); ev; constructor; auto.
+    - have Hctx': stamped_ctx getStampTable (V :: Γ). by constructor.
+      specialize (H Hctx'); ev; constructor; auto.
+    - have Hctx': stamped_ctx getStampTable (T1 :: Γ). by constructor.
+      specialize (H Hctx'); ev; constructor; auto.
+    - have Hctx': stamped_ctx getStampTable (T1 :: Γ). by constructor.
+      specialize (H Hctx'); ev; constructor; eauto. by eapply stamped_ren.
+    - have Hctx': stamped_ctx getStampTable (T1.|[ren (+1)] :: Γ).
+        constructor => //. by eapply stamped_ren in f.
+      specialize (H Hctx'); ev; constructor; eauto.
+    - have Hctx': stamped_ctx getStampTable (T2.|[ren (+1)] :: Γ).
+        constructor => //. by eapply stamped_ren in f.
+      specialize (H Hctx); specialize (H0 Hctx');
+        ev; constructor; eauto.
   Qed.
 
 End syntyping.
