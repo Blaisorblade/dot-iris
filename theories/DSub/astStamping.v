2c7a7cd57580289951939220a19652386d244932
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-03-10 00:17:51 +0100

    Split DSub/astStamping, following Dot/stampedness

--- theories/DSub/astStamping.v
+++ theories/DSub/astStamping.v
@@ -2,7 +2,7 @@
     way, without involving Iris. *)
 From stdpp Require Import gmap.
 From D Require Import tactics.
-From D.DSub Require Import syn operational synLemmas typeExtractionSyn.
+From D.DSub Require Import syn operational synLemmas typeExtractionSyn stampedness.
 
 Set Primitive Projections.
 Set Implicit Arguments.
@@ -53,102 +53,12 @@ unstamp_ty g (T: ty): ty :=
   | TNat => T
   end.
 
-Record Traversal {travStateT: Type} :=
-  {
-    upS: travStateT → travStateT;
-    varP: travStateT → nat → Prop;
-    vtyP: travStateT → ty → Prop;
-    vstampP: travStateT → vls → stamp → Prop;
-  }.
-Global Arguments Traversal _: clear implicits.
-
-Definition is_unstamped_trav: Traversal unit :=
-  {|
-    upS := id;
-    varP := λ s n, True;
-    vtyP := λ s T, True;
-    vstampP := λ s vs s, False;
-  |}.
-
-Definition is_stamped_trav: Traversal (nat * stys) :=
-  {|
-    upS := λ '(n, g), (S n, g);
-    varP := λ ts n, True;
-    vtyP := λ ts T, False;
-    vstampP := λ '(n, g) vs s, length vs = n ∧ ∃ T, g !! s = Some T ∧ nclosed T n;
-  |}.
-
-Section fold.
-  Context `(trav: Traversal travStateT).
-
-  Inductive forall_traversal_vl: travStateT → vl → Prop :=
-    | trav_var_vl ts i: trav.(varP) ts i → forall_traversal_vl ts (var_vl i)
-    | trav_vabs ts t: forall_traversal_tm (trav.(upS) ts) t →
-                          forall_traversal_vl ts (vabs t)
-    | trav_vnat ts n: forall_traversal_vl ts (vnat n)
-    | trav_vty ts T:
-        forall_traversal_ty ts T →
-        trav.(vtyP) ts T →
-        forall_traversal_vl ts (vty T)
-    | trav_vstamp ts vs s:
-          trav.(vstampP) ts vs s →
-          (* This is weird, but apparently we get away without checking that
-              these values are syntactic. *)
-          (* Forall (forall_traversal_vl ts) vs → *)
-          forall_traversal_vl ts (vstamp vs s)
-  with
-  forall_traversal_tm: travStateT → tm → Prop :=
-  | trav_tv ts v: forall_traversal_vl ts v → forall_traversal_tm ts (tv v)
-  | trav_tapp ts t1 t2:
-      forall_traversal_tm ts t1 →
-      forall_traversal_tm ts t2 →
-      forall_traversal_tm ts (tapp t1 t2)
-  | trav_tskip ts t:
-      forall_traversal_tm ts t →
-      forall_traversal_tm ts (tskip t)
-  with
-  forall_traversal_ty: travStateT → ty → Prop :=
-  | trav_TAll ts T1 T2:
-      forall_traversal_ty ts T1 →
-      forall_traversal_ty (trav.(upS) ts) T2 →
-      forall_traversal_ty ts (TAll T1 T2)
-  | trav_TMem ts T1 T2:
-      forall_traversal_ty ts T1 →
-      forall_traversal_ty ts T2 →
-      forall_traversal_ty ts (TTMem T1 T2)
-  | trav_TSel ts v:
-      forall_traversal_vl ts v →
-      forall_traversal_ty ts (TSel v)
-  | trav_TNat ts: forall_traversal_ty ts TNat
-    .
-End fold.
-
-Notation is_unstamped_tm := (forall_traversal_tm is_unstamped_trav ()).
-Notation is_unstamped_vl := (forall_traversal_vl is_unstamped_trav ()).
-Notation is_unstamped_ty := (forall_traversal_ty is_unstamped_trav ()).
-Notation is_stamped_tm n g := (forall_traversal_tm is_stamped_trav (n, g)).
-Notation is_stamped_vl n g := (forall_traversal_vl is_stamped_trav (n, g)).
-Notation is_stamped_ty n g := (forall_traversal_ty is_stamped_trav (n, g)).
-
-Global Arguments upS /.
-Global Arguments varP /.
-Global Arguments vtyP /.
-Global Arguments vstampP /.
-
 (** XXX this formulation might be inconvenient: storing the correct n in the map might be preferable. *)
 Definition is_stamped_gmap g: Prop := ∀ s T, g !! s = Some T → ∃ n, is_stamped_ty n g T.
 
 Notation stamps_tm n e__u g e__s := (unstamp_tm g e__s = e__u ∧ is_unstamped_tm e__u ∧ is_stamped_tm n g e__s).
 Notation stamps_vl n v__u g v__s := (unstamp_vl g v__s = v__u ∧ is_unstamped_vl v__u ∧ is_stamped_vl n g v__s).
 Notation stamps_ty n T__u g T__s := (unstamp_ty g T__s = T__u ∧ is_unstamped_ty T__u ∧ is_stamped_ty n g T__s).
-
-Lemma stamped_idsσ_ren g m n r: Forall (is_stamped_vl m g) (idsσ n).|[ren r].
-Proof.
-  elim: n m r => [|n IHn] m r //=.
-  repeat constructor => //=. asimpl. apply IHn.
-Qed.
-
-Hint Constructors forall_traversal_vl forall_traversal_ty forall_traversal_tm.
 
 (* Unused. *)
 (* Lemma stamped_idsσ g m n: Forall (is_stamped_vl m g) (idsσ n). *)
@@ -169,69 +79,6 @@ Qed.
 
 Lemma exists_stamped_vstamp vs s n g: is_unstamped_vl (vstamp vs s) → nclosed_vl (vstamp vs s) n → { v' & { g' | stamps_vl n (vstamp vs s) g' v' ∧ g ⊆ g' } }.
 Proof. intro H. exfalso. by inversion H. Qed.
-
-Lemma not_stamped_vty g n T:
-  ¬ (is_stamped_vl n g (vty T)).
-Proof. by inversion 1. Qed.
-
-Lemma is_stamped_vty_mono g1 g2 n T:
-  g1 ⊆ g2 →
-  is_stamped_vl n g1 (vty T) →
-  is_stamped_vl n g2 (vty T).
-Proof. intros; exfalso. by eapply not_stamped_vty. Qed.
-
-Lemma is_stamped_vstamp_mono g1 g2 n s vs:
-  g1 ⊆ g2 →
-  is_stamped_vl n g1 (vstamp vs s) →
-  is_stamped_vl n g2 (vstamp vs s).
-Proof.
-  inversion 2; subst; simpl in *; ev.
-  repeat econstructor => //=. by eapply map_subseteq_spec.
-Qed.
-
-Ltac with_is_stamped tac :=
-  match goal with
-    | H: is_stamped_ty _ _ _ |- _ => tac H
-    | H: is_stamped_tm _ _ _ |- _ => tac H
-    | H: is_stamped_vl _ _ _ |- _ => tac H
-  end.
-
-Lemma is_stamped_mono_mut:
-  (∀ e__s g1 g2 n,
-       g1 ⊆ g2 →
-       is_stamped_tm n g1 e__s →
-       is_stamped_tm n g2 e__s) ∧
-  (∀ v__s g1 g2 n,
-      g1 ⊆ g2 →
-      is_stamped_vl n g1 v__s →
-      is_stamped_vl n g2 v__s) ∧
-  (∀ T__s g1 g2 n,
-      g1 ⊆ g2 →
-      is_stamped_ty n g1 T__s →
-      is_stamped_ty n g2 T__s).
-Proof.
-  apply syntax_mut_ind => *; with_is_stamped inverse;
-    by [ eapply is_stamped_vstamp_mono | constructor; cbn in *; eauto].
-Qed.
-
-(** Tactic to split a lemma proven by mutual induction into its pieces. *)
-Ltac unmut_lemma H := destruct H; ev; eauto.
-
-Lemma is_stamped_mono_tm g1 g2 n e__s:
-  g1 ⊆ g2 →
-  is_stamped_tm n g1 e__s →
-  is_stamped_tm n g2 e__s.
-Proof. unmut_lemma is_stamped_mono_mut. Qed.
-Lemma is_stamped_mono_vl g1 g2 n v__s:
-  g1 ⊆ g2 →
-  is_stamped_vl n g1 v__s →
-  is_stamped_vl n g2 v__s.
-Proof. unmut_lemma is_stamped_mono_mut. Qed.
-Lemma is_stamped_mono_ty g1 g2 n T__s:
-  g1 ⊆ g2 →
-  is_stamped_ty n g1 T__s →
-  is_stamped_ty n g2 T__s.
-Proof. unmut_lemma is_stamped_mono_mut. Qed.
 
 Lemma stamps_unstamp_vstamp_mono g1 g2 n v__u vs s:
   g1 ⊆ g2 →
