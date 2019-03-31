06de12919b82f2092bb7b225ecc64e38221a28ad
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-03-31 01:20:52 +0100

    saved_interp.v: Shorten proofs

--- theories/saved_interp.v
+++ theories/saved_interp.v
@@ -17,17 +17,12 @@ Section saved_interp.
 
   Definition saved_interp_own (γ : gname) Φ :=
     saved_anything_own
-      (F := vls -c> vl -c> ▶ ∙) γ (λ vs v, CofeMor Next (Φ vs v)).
+      (F := vls -c> vl -c> ▶ ∙) γ (λ vs v, Next (Φ vs v)).
 
   Instance saved_interp_own_contractive γ : Contractive (saved_interp_own γ).
   Proof.
-    intros n X Y HXY.
-    rewrite /saved_interp_own /saved_anything_own /=.
-    f_equiv. apply to_agree_ne; f_equiv.
-    intros x y.
-    apply Next_contractive.
-    destruct n; simpl in *; auto.
-    apply HXY.
+    rewrite /saved_interp_own /saved_anything_own => /= n X Y HXY.
+    (do 3 f_equiv) => ??. f_contractive. exact: HXY.
   Qed.
 
   Lemma saved_interp_alloc_strong (G : gset gname) Φ :
@@ -41,20 +36,17 @@ Section saved_interp.
   Lemma saved_interp_agree γ Φ Ψ vs v :
     saved_interp_own γ Φ -∗ saved_interp_own γ Ψ -∗ ▷ (Φ vs v ≡ Ψ vs v).
   Proof.
-    unfold saved_pred_own. iIntros "#HΦ #HΨ /=". iApply bi.later_equivI.
+    iIntros "#HΦ #HΨ /=". iApply bi.later_equivI.
     iDestruct (saved_anything_agree with "HΦ HΨ") as "Heq".
     rewrite bi.ofe_fun_equivI; iSpecialize ("Heq" $! vs).
-    by rewrite bi.ofe_fun_equivI; iSpecialize ("Heq" $! v); simpl.
+    by rewrite bi.ofe_fun_equivI; iSpecialize ("Heq" $! v).
   Qed.
 
   Lemma saved_interp_impl γ Φ Ψ vs v :
     saved_interp_own γ Φ -∗ saved_interp_own γ Ψ -∗ □ (▷ Φ vs v -∗ ▷ Ψ vs v).
   Proof.
-    unfold saved_pred_own. iIntros "#HΦ #HΨ /= !# H1".
-    iDestruct (saved_anything_agree with "HΦ HΨ") as "Heq".
-    rewrite bi.ofe_fun_equivI; iSpecialize ("Heq" $! vs).
-    rewrite bi.ofe_fun_equivI; iSpecialize ("Heq" $! v); simpl.
-    rewrite bi.later_equivI.
+    iIntros "#HΦ #HΨ /= !# H1".
+    iPoseProof (saved_interp_agree _ _ _ vs v with "HΦ HΨ") as "Heq".
     by iNext; iRewrite -"Heq".
   Qed.
 End saved_interp.
