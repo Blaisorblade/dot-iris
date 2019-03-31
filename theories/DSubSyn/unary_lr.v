516f9687ef8779b64946c871b71a52406f661e12
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-03-31 22:08:33 +0200

    D<:: Interpret nested types recursively instead of using translation
    - Prove core typing lemmas for D<:.
    - Make type constructor for functions contractive for D<: as well.
    - Prove agreement from vl_has_semtype.
    - For interp_rec1, in this version, avoid proving both non-expansiveness and
      contractiveness, Iris is smart enough. Nor need we pack non-expansiveness
      together with the function.
    - DSubSyn: require no Iris resources
    - Type members: no present subtyping (since we don't support type
      `v.A :: L .. U`).
    - Use implication, not wand, to help swaps with later.
      We're looking at what it takes to swap implication and later, and we haven't
      considered whether that works for wands as well, so stick to implication.
    - Add rule for type member introduction
    - Prove adequacy
    - Use new lemmas to strip laterN from pure hypotheses
    
      See https://gitlab.mpi-sws.org/iris/iris/issues/237 and
      fce89fca12e705a96f2ec11b2aeea61a16828e71.

---
+++ theories/DSubSyn/unary_lr.v
@@ -0,0 +1,335 @@
+From iris.proofmode Require Import tactics.
+From D Require Export tactics.
+From D.DSub Require Export operational.
+
+(** Deduce types from variable names, like on paper, for readability and to help
+    type inference for some overloaded operations (e.g. substitution). *)
+Implicit Types
+         (L T U: ty) (v: vl) (e: tm)
+         (Γ : ctx) (ρ : vls).
+
+(** An ad-hoc variant of solve_proper that seems to work better when defining
+      proper higher-order functions. In particular, using intro allows showing that a
+      lambda abstraction is proper if its body is proper.
+      Its implementation can also prove [f1 x ≡ f2 x] from [H : f1 ≡ f2]:
+      neither f_equiv nor rewrite deal with that, but [apply H] does. *)
+Ltac solve_proper_ho_core tac :=
+  solve [repeat intro; cbn; repeat tac (); cbn in *;
+  repeat match goal with H : _ ≡{_}≡ _|- _ => apply H end].
+Ltac solve_proper_ho := solve_proper_ho_core ltac:(fun _ => f_equiv).
+Ltac solve_contractive_ho := solve_proper_ho_core ltac:(fun _ => f_contractive || f_equiv).
+
+(** The logical relation core is the [interp], interprets *open* types into
+    predicates over *closed* values. Hence, [interp T ρ v] uses its argument [ρ]
+    to interpret anything contained in T, but not things contained in v.
+
+    Semantic judgements must apply instead to open terms/value/paths; therefore,
+    they are defined using closing substitution on arguments of [interp].
+
+    Similar comments apply to [def_interp].
+
+    Additionally, both apply to *translated* arguments, hence they only expect
+    [dtysem] and not [dtysyn] for type member definitions.
+ *)
+
+(* The only point of these instances is to select Σ uniquely. *)
+Class dsubSynG (Σ: gFunctors) := DsubSynG {}.
+
+Instance dsubsynG_irisG `{!dsubSynG Σ}: irisG dsub_lang Σ := {
+  state_interp σ κs _ := True%I;
+  fork_post _ := True%I;
+}.
+
+(* Use Program without its extended pattern-matching compiler; we only need
+    its handling of coercions. *)
+Unset Program Cases.
+
+Section logrel.
+  Context `{!dsubSynG Σ}.
+
+  Notation D := (vl -c> iProp Σ).
+  Implicit Types (interp : envD Σ) (φ : D).
+
+  Program Definition interp_expr : envD Σ -n> vls -c> tm -c> iProp Σ :=
+    λne interp, λ ρ t, WP t {{ interp ρ }} %I.
+  Solve All Obligations with solve_proper_ho.
+  Global Arguments interp_expr /.
+
+  Definition interp_nat : envD Σ := λ ρ v, (∃ n, ⌜v = vnat n⌝) %I.
+  Global Arguments interp_nat /.
+
+  Definition interp_top : envD Σ := λ ρ v, ⌜ nclosed_vl v 0 ⌝%I.
+  Global Arguments interp_top /.
+
+  Definition interp_bot : envD Σ := λ ρ v, False%I.
+  Global Arguments interp_bot /.
+
+  Program Definition interp_later: envD Σ -n> envD Σ :=
+    λne interp, λ ρ v, (⌜ nclosed_vl v 0 ⌝ ∗ ▷ interp ρ v) % I.
+  Solve All Obligations with solve_proper_ho.
+  Global Arguments interp_later /.
+
+  (* Paolo: This definition is contractive (similarly to what's useful for
+     equi-recursive types).
+     However, I am not sure we need this; it'd be good to
+     write an example where this makes a difference.
+     I think that would be something like
+     nu x. { T = TNat; U = x.T -> x.T }:
+     mu (x: {T <: TNat; U <: x.T -> TNat}).
+     If the function type constructor is not contractive but only non-expansive,
+     typechecking this example needs to establish x.T <: TNat having in context
+     only x: {T <: TNat; U <: x.T -> TNat}.
+    *)
+
+  Program Definition interp_forall: envD Σ -n> envD Σ -n> envD Σ :=
+    λne interp1 interp2, λ ρ v,
+    (⌜ nclosed_vl v 0 ⌝ ∗
+       ∃ t, ⌜ v = vabs t ⌝ ∗
+       □ ▷ ∀ w, interp1 ρ w → interp_expr interp2 (w :: ρ) t.|[w/])%I.
+  Solve All Obligations with solve_proper_ho.
+  Global Arguments interp_forall /.
+
+  Program Definition vl_has_semtype : (ty -c> envD Σ) -n> vl -c> D -n> iProp Σ :=
+    λne rinterp, λ v, λne φ,
+    (∃ T, ⌜ v = vty T ⌝ ∧ ∀ w, ▷ (φ w ≡ rinterp T [] w))%I.
+  Solve All Obligations with solve_proper_ho.
+  Global Arguments vl_has_semtype /.
+  Notation "[ rinterp ] v ↗ φ" := (vl_has_semtype rinterp v φ) (at level 20).
+
+  Lemma vl_has_semtype_agree rinterp v (φ1 φ2 : D):
+    ([ rinterp ] v ↗ φ1 →
+     [ rinterp ] v ↗ φ2 →
+     ∀ w, ▷ (φ1 w ≡ φ2 w))%I.
+  Proof.
+    iIntros "/= #H1 #H2" (w).
+    iDestruct "H1" as (T1 ->) "#Heq1"; iDestruct "H2" as (T ?) "#Heq2"; injectHyps.
+    iNext.
+    by iRewrite ("Heq1" $! w); iRewrite ("Heq2" $! w).
+  Qed.
+
+  Global Instance vl_has_semtype_contractive : Contractive vl_has_semtype.
+  Proof. solve_contractive_ho. Qed.
+
+  Program Definition interp_tmem :
+    (ty -c> envD Σ) -n> envD Σ -n> envD Σ -n> envD Σ :=
+    λne rinterp interpL interpU, λ ρ v,
+    (⌜ nclosed_vl v 0 ⌝ ∗  ∃ φ, [ rinterp ] v ↗ φ ∗
+       □ ((∀ v, ⌜ nclosed_vl v 0 ⌝ → ▷ interpL ρ v → ▷ □ φ v) ∗
+          (∀ v, ⌜ nclosed_vl v 0 ⌝ → ▷ □ φ v → ▷ interpU ρ v)))%I.
+  Solve All Obligations with solve_proper_ho.
+  Global Arguments interp_tmem /.
+
+  Global Instance interp_tmem_contractive : Contractive interp_tmem.
+  Proof. solve_contractive_ho. Qed.
+
+  Program Definition interp_sel: (ty -c> envD Σ) -n> vl -c> envD Σ :=
+    λne rinterp, λ w ρ v,
+    (⌜ nclosed_vl v 0 ⌝ ∧ (∃ ϕ, [rinterp] w.[to_subst ρ] ↗ ϕ ∧ ▷ □ ϕ v))%I.
+  Solve All Obligations with solve_proper_ho.
+  Global Arguments interp_sel /.
+  Global Instance interp_sel_contractive : Contractive interp_sel.
+  Proof. solve_contractive_ho. Qed.
+
+  (* This is structurally recursive, so must be a normal function.
+     Non-structurally recursive calls must happen under [▷] and use [rinterp]. *)
+  Program Fixpoint interp_rec0 (rinterp: ty -c> envD Σ) (T: ty): envD Σ :=
+    match T with
+    | TLater T => interp_later (interp_rec0 rinterp T)
+    | TTMem L U => interp_tmem rinterp (interp_rec0 rinterp L) (interp_rec0 rinterp U)
+    | TNat => interp_nat
+    | TAll T1 T2 => interp_forall (interp_rec0 rinterp T1) (interp_rec0 rinterp T2)
+    | TSel w => interp_sel rinterp w
+    | TTop => interp_top
+    | TBot => interp_bot
+    end%I.
+  (* This is a non-expansive function so can't be a fixpoint, but it can call it. *)
+  Definition interp_rec1 : (ty -c> envD Σ) -> ty -c> envD Σ := interp_rec0.
+
+  (* solve_contractive is really not happy about checking this code. *)
+  Global Instance interp_rec1_contractive : Contractive interp_rec1.
+  Proof.
+    move => ???? T /=; induction T.
+    (* Generic but slow *)
+    (* all: time (cbn -[interp_forall interp_tmem interp_sel interp_nat] in *; trivial;
+      repeat (match goal with
+                H : _ ≡{_}≡ _|- _ => apply dist_S, H || apply H
+              end || (f_contractive; cbn -[interp_forall interp_tmem interp_sel interp_nat] in * ) || f_equiv)). *)
+    all: cbn -[interp_later interp_forall interp_tmem interp_sel interp_nat];
+      try by [apply interp_sel_contractive];
+      rewrite // ?IHT ?IHT2; f_equiv; rewrite IHT1 //.
+    - f_equiv. by f_contractive.
+  Qed.
+
+  Program Lemma fixpoint_interp_rec1_eq:
+    fixpoint interp_rec1 ≡ interp_rec1 (fixpoint interp_rec1).
+  Proof. exact: (fixpoint_unfold (interp_rec1)). Qed.
+
+  Program Definition interp: ty -c> envD Σ := fixpoint interp_rec1.
+  Notation "⟦ T ⟧" := (interp T).
+
+  Global Instance dsubInterpΣ : dsubInterpG Σ := DsubInterpG _ interp.
+
+  Lemma fixpoint_interp_eq1 T: interp T ≡ interp_rec1 interp T.
+  Proof. apply fixpoint_interp_rec1_eq. Qed.
+  Lemma fixpoint_interp_eq2 T ρ: interp T ρ ≡ interp_rec1 interp T ρ.
+  Proof. apply fixpoint_interp_rec1_eq. Qed.
+  Lemma fixpoint_interp_eq3 T ρ v: interp T ρ v ≡ interp_rec1 interp T ρ v.
+  Proof. apply fixpoint_interp_rec1_eq. Qed.
+
+  Ltac rewrite_interp := repeat first [rewrite fixpoint_interp_eq3 | progress (repeat f_equiv; rewrite ?fixpoint_interp_eq1 //=) | move => ? /= ].
+  Lemma interp_TAll T1 T2 ρ v: ⟦ TAll T1 T2 ⟧ ρ v ≡ interp_forall ⟦ T1 ⟧ ⟦ T2 ⟧ ρ v.
+  Proof. rewrite_interp. Qed.
+  Lemma interp_TNat ρ v: ⟦ TNat ⟧ ρ v ≡ interp_nat ρ v.
+  Proof. rewrite_interp. Qed.
+  Lemma interp_TLater T1 ρ v: ⟦ TLater T1 ⟧ ρ v ≡ interp_later ⟦ T1 ⟧ ρ v.
+  Proof. rewrite_interp. Qed.
+  Lemma interp_TTMem T1 T2 ρ v: ⟦ TTMem T1 T2 ⟧ ρ v ≡ interp_tmem interp ⟦ T1 ⟧ ⟦ T2 ⟧ ρ v.
+  Proof. rewrite_interp. Qed.
+  Lemma interp_TSel ρ v w: ⟦ TSel w ⟧ ρ v ≡ interp_sel interp w ρ v.
+  Proof. rewrite_interp. Qed.
+  Lemma interp_TTop ρ v: ⟦ TTop ⟧ ρ v ≡ interp_top ρ v.
+  Proof. rewrite_interp. Qed.
+  Lemma interp_TBot ρ v: ⟦ TBot ⟧ ρ v ≡ interp_bot ρ v.
+  Proof. rewrite_interp. Qed.
+End logrel.
+
+Notation "⟦ T ⟧" := (interp T).
+Notation "⟦ T ⟧ₑ" := (interp_expr (interp T)).
+
+(** Unfold uses of interp, but only if the argument allows progress. *)
+Ltac unfold_interp :=
+  rewrite
+    ?interp_TLater ?interp_TAll ?interp_TNat ?interp_TTMem
+    ?interp_TSel ?interp_TTop ?interp_TBot /=.
+
+Ltac setoid_unfold_interp :=
+  try setoid_rewrite interp_TAll;
+  try setoid_rewrite interp_TLater;
+  try setoid_rewrite interp_TSel;
+  try setoid_rewrite interp_TNat;
+  try setoid_rewrite interp_TTMem;
+  try setoid_rewrite interp_TTop;
+  try setoid_rewrite interp_TBot;
+  cbn.
+
+Section logrel_part2.
+  Context `{!dsubSynG Σ}.
+
+  Global Instance interp_persistent T ρ v :
+    Persistent (⟦ T ⟧ ρ v).
+  Proof. revert v ρ; induction T => w ρ; unfold_interp; try apply _. Qed.
+
+  (* XXX here we needn't add a variable to the scope of its own type. But that won't hurt. *)
+  Fixpoint interp_env (Γ : ctx) (vs : vls) : iProp Σ :=
+    match Γ with
+    | nil => ⌜vs = nil⌝
+    | T :: Γ' =>
+      match vs with
+      | nil => False
+      | v :: ρ => interp_env Γ' ρ ∗ ⟦ T ⟧ (v::ρ) v
+      end
+    end%I.
+
+  Notation "⟦ Γ ⟧*" := (interp_env Γ).
+
+  Global Instance interp_env_persistent Γ ρ :
+    Persistent (⟦ Γ ⟧* ρ).
+  Proof. elim: Γ ρ => [|τ Γ IHΓ] [|v ρ]; apply _. Qed.
+
+  Notation "⟦ T ⟧ₑ" := (interp_expr (interp T)).
+
+  (* Really needed? Try to stop using it. *)
+  Definition ivtp Γ T v : iProp Σ := (⌜ nclosed_vl v (length Γ) ⌝ ∗ □∀ ρ, ⟦Γ⟧* ρ → ⟦T⟧ ρ v.[to_subst ρ])%I.
+  Global Arguments ivtp /.
+
+  Definition ietp Γ T e : iProp Σ := (⌜ nclosed e (length Γ) ⌝ ∗ □∀ ρ, ⟦Γ⟧* ρ → ⟦T⟧ₑ ρ (e.|[to_subst ρ]))%I.
+  Global Arguments ietp /.
+  Notation "Γ ⊨ e : T" := (ietp Γ T e) (at level 74, e, T at next level).
+
+  Lemma ietp_closed Γ T e: (Γ ⊨ e : T → ⌜ nclosed e (length Γ) ⌝)%I.
+  Proof. iIntros "[$ _]". Qed.
+
+  Definition step_indexed_ietp Γ T e i: iProp Σ :=
+    (⌜ nclosed e (length Γ) ⌝ ∗ □∀ ρ, ⟦Γ⟧* ρ → ▷^i ⟦T⟧ₑ ρ (e.|[to_subst ρ]))%I.
+  Global Arguments step_indexed_ietp /.
+  Notation "Γ ⊨ e : T , i" := (step_indexed_ietp Γ T e i) (at level 74, e, T at next level).
+
+  Lemma step_indexed_ietp_closed Γ T e i: (Γ ⊨ e : T, i → ⌜ nclosed e (length Γ) ⌝)%I.
+  Proof. iIntros "[$ _]". Qed.
+
+  (** Indexed Subtyping. Defined on closed values. We must require closedness
+      explicitly, since closedness now does not follow from being well-typed later. *)
+  Definition step_indexed_ivstp Γ T1 T2 i j: iProp Σ :=
+    (□∀ ρ v, ⌜ nclosed_vl v 0 ⌝ → ⟦Γ⟧*ρ → (▷^i ⟦T1⟧ ρ v) → ▷^j ⟦T2⟧ ρ v)%I.
+  Global Arguments step_indexed_ivstp /.
+
+  Global Instance ietp_persistent Γ T e : Persistent (ietp Γ T e) := _.
+  Global Instance step_indexed_ietp_persistent Γ T e i : Persistent (step_indexed_ietp Γ T e i) := _.
+  Global Instance step_indexed_ivstp_persistent Γ T1 T2 i j : Persistent (step_indexed_ivstp Γ T1 T2 i j) := _.
+End logrel_part2.
+
+Notation "⟦ Γ ⟧*" := (interp_env Γ).
+
+(** Expression typing *)
+Notation "Γ ⊨ e : T" := (ietp Γ T e) (at level 74, e, T at next level).
+(** Indexed expression typing *)
+Notation "Γ ⊨ e : T , i" := (step_indexed_ietp Γ T e i) (at level 74, e, T at next level).
+
+Notation "Γ '⊨' '[' T1 ',' i ']' '<:' '[' T2 ',' j ']'" := (step_indexed_ivstp Γ T1 T2 i j) (at level 74, T1, T2 at next level).
+
+Section logrel_lemmas.
+  Context `{!dsubSynG Σ}.
+
+  Lemma iterate_TLater_later i T ρ v:
+    nclosed_vl v 0 →
+    ⟦ iterate TLater i T ⟧ ρ v ≡ (▷^i ⟦ T ⟧ ρ v)%I.
+  Proof.
+    elim: i => [|i IHi] // => Hcl.
+    rewrite iterate_S /=; unfold_interp; rewrite IHi //.
+    iSplit; by [> iIntros "#[_ $]" | iIntros "$"].
+  Qed.
+
+  Context Γ.
+
+  Lemma semantic_typing_uniform_step_index T e i:
+    Γ ⊨ e : T ⊢ Γ ⊨ e : T,i.
+  Proof.
+    iIntros "[$ #H] !>" (ρ) "#HΓ".
+    iInduction i as [|i] "IHi". by iApply "H". iApply "IHi".
+  Qed.
+
+  Lemma interp_v_closed T w ρ: ⟦ T ⟧ ρ w ⊢ ⌜ nclosed_vl w 0 ⌝.
+  Proof.
+    iInduction T as [] "IHT" forall (ρ w); iIntros "#HT //="; unfold_interp;
+      by [iDestruct "HT" as "[% _]"|iDestruct "HT" as (n) "->"|].
+  Qed.
+
+  Lemma interp_env_len_agree ρ:
+    ⟦ Γ ⟧* ρ ⊢ ⌜ length ρ = length Γ ⌝.
+  Proof.
+    iInduction Γ as [|τ Γ'] "IHΓ" forall (ρ); destruct ρ => //=.
+    iIntros "#[HG Hv]".
+    by iDestruct ("IHΓ" $! ρ with "HG") as "->".
+  Qed.
+
+  Lemma interp_env_ρ_closed ρ: ⟦ Γ ⟧* ρ ⊢ ⌜ cl_ρ ρ ⌝.
+  Proof.
+    iInduction Γ as [|τ Γ'] "IHΓ" forall (ρ); destruct ρ => //=.
+    iIntros "[#HG #HT]".
+    iPoseProof (interp_v_closed with "HT") as "%".
+    iPoseProof ("IHΓ" with "HG") as "%".
+    iPureIntro; by constructor.
+  Qed.
+
+  Lemma Sub_Refl T i : Γ ⊨ [T, i] <: [T, i].
+  Proof. by iIntros "/= !> **". Qed.
+
+  Lemma Sub_Trans T1 T2 T3 i1 i2 i3 : Γ ⊨ [T1, i1] <: [T2, i2] -∗
+                                      Γ ⊨ [T2, i2] <: [T3, i3] -∗
+                                      Γ ⊨ [T1, i1] <: [T3, i3].
+  Proof.
+    iIntros "#Hsub1 #Hsub2 /= !> * % #Hg #HT".
+    iApply "Hsub2" => //. by iApply "Hsub1".
+  Qed.
+
+End logrel_lemmas.
