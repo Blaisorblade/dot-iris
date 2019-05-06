405627151bf6ceaefd2845aece988e911c053f4e
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-05-06 20:39:31 +0200

    Replace current definition of same_skel with framework instance

--- theories/Dot/skeleton.v
+++ theories/Dot/skeleton.v
@@ -1,4 +1,5 @@
-(** Define translation from syntactic terms/values to semantic ones, following Sec. 3.2 of the PDF. *)
+(** Define matching between terms which only differ in type members,
+    following Sec. 3.5 of the PDF. *)
 From iris.program_logic Require Import ectx_lifting ectx_language ectxi_language.
 From D Require Import tactics.
 From D.Dot Require Import operational traversals.
@@ -12,75 +13,11 @@ Definition same_skeleton_trav: Traversal unit :=
     dtyP := λ s tm1 tm2, True;
   |}.
 
-Notation same_skeleton_tm := (forall_traversal_tm same_skeleton_trav ()).
-Notation same_skeleton_vl := (forall_traversal_vl same_skeleton_trav ()).
-Notation same_skeleton_dm := (forall_traversal_dm same_skeleton_trav ()).
-Notation same_skeleton_path := (forall_traversal_path same_skeleton_trav ()).
-Notation same_skeleton_ty := (forall_traversal_ty same_skeleton_trav ()).
-
-Fixpoint same_skel_tm (t1 t2: tm): Prop :=
-  match (t1, t2) with
-  | (tv v1, tv v2) => same_skel_vl v1 v2
-  | (tapp t11 t12, tapp t21 t22) =>
-    same_skel_tm t11 t21 ∧ same_skel_tm t12 t22
-  | (tproj t1 l1, tproj t2 l2) =>
-    same_skel_tm t1 t2 ∧ l1 = l2
-  | (tskip t1, tskip t2) =>
-    same_skel_tm t1 t2
-  | _ => False
-  end
-with
-same_skel_vl (v1 v2: vl): Prop :=
-  match (v1, v2) with
-  | (var_vl i1, var_vl i2) => i1 = i2
-  | (vabs t1, vabs t2) => same_skel_tm t1 t2
-  | (vobj ds1, vobj ds2) =>
-    let fix same_skel_dms (ds1 ds2: dms): Prop :=
-        match (ds1, ds2) with
-        | (nil, nil) => True
-        | (cons (l1, d1) ds1, cons (l2, d2) ds2) =>
-          l1 = l2 ∧ same_skel_dm d1 d2 ∧ same_skel_dms ds1 ds2
-        | _ => False
-        end
-    in same_skel_dms ds1 ds2
-  | (vnat n1, vnat n2) => n1 = n2
-  | _ => False
-  end
-with
-same_skel_dm (d1 d2: dm): Prop :=
-  match (d1, d2) with
-  | (dtysyn T1, dtysem σ2 γ2) =>
-    (* Only nontrivial case *)
-    True
-  | (dvl v1, dvl v2) => same_skel_vl v1 v2
-  | _ => False
-  end.
-Fixpoint same_skel_path (p1 p2: path): Prop :=
-  match (p1, p2) with
-  | (pv v1, pv v2) => same_skel_vl v1 v2
-  | (pself p1 l1, pself p2 l2) => same_skel_path p1 p2 ∧ l1 = l2
-  | _ => False
-  end.
-Fixpoint same_skel_ty (T1 T2: ty): Prop :=
-  match (T1, T2) with
-  | (TTop, TTop) => True
-  | (TBot, TBot) => True
-  | (TAnd T11 T12, TAnd T21 T22) =>
-    same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-  | (TOr T11 T12, TOr T21 T22) =>
-    same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-  | (TLater T1, TLater T2) =>
-    same_skel_ty T1 T2
-  | (TAll T11 T12, TAll T21 T22) =>
-    same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-  | (TMu T1, TMu T2) =>
-    same_skel_ty T1 T2
-  | (TVMem l1 T1, TVMem l2 T2) => l1 = l2 ∧ same_skel_ty T1 T2
-  | (TTMem l1 T11 T12, TTMem l2 T21 T22) => l1 = l2 ∧ same_skel_ty T11 T21 ∧ same_skel_ty T12 T22
-  | (TSel p1 l1, TSel p2 l2) => same_skel_path p1 p2 ∧ l1 = l2
-  | (TNat, TNat) => True
-  | _ => False
-  end.
+Notation same_skel_tm := (forall_traversal_tm same_skeleton_trav ()).
+Notation same_skel_vl := (forall_traversal_vl same_skeleton_trav ()).
+Notation same_skel_dm := (forall_traversal_dm same_skeleton_trav ()).
+Notation same_skel_path := (forall_traversal_path same_skeleton_trav ()).
+Notation same_skel_ty := (forall_traversal_ty same_skeleton_trav ()).
 
 Fixpoint same_skel_ectx K K' :=
   match K, K' with
@@ -111,26 +48,25 @@ Proof.
   induction Ks using rev_ind => e t'; simpl.
   {intros ?. exists [], t'; simpl; repeat split; auto using same_skel_list_ectx_empty. }
   rewrite fill_app /= => HKe.
-  destruct x; destruct t'; simpl in *; try done.
-  - destruct HKe as [HKe Ht'].
-    apply IHKs in HKe.
-    destruct HKe as (Ks' & e' & -> & He' & HKs').
+  destruct x; destruct t';
+    inversion HKe; subst;
+    simpl in *; try done.
+  - apply IHKs in H3.
+    destruct H3 as (Ks' & e' & -> & He' & HKs').
     exists (Ks' ++ [AppLCtx t'2]), e'.
     rewrite fill_app /=; repeat split; auto using same_skel_list_ectx_app.
-  - destruct HKe as [Ht HKe'].
-    destruct t'1; try done.
-    apply IHKs in HKe'.
-    destruct HKe' as (Ks' & e' & -> & He' & HKs').
+  - destruct t'1; inversion_clear H3; try done.
+    apply IHKs in H5.
+    destruct H5 as (Ks' & e' & -> & He' & HKs').
     exists (Ks' ++ [AppRCtx v]), e'.
     rewrite fill_app /=; repeat split; auto using same_skel_list_ectx_app.
-  - destruct HKe as [HKe <-].
-    apply IHKs in HKe.
-    destruct HKe as (Ks' & e' & -> & He' & HKs').
-    exists (Ks' ++ [ProjCtx l]), e'.
+  - apply IHKs in H1.
+    destruct H1 as (Ks' & e' & -> & He' & HKs').
+    exists (Ks' ++ [ProjCtx l0]), e'.
     rewrite fill_app /=; repeat split; trivial.
     apply same_skel_list_ectx_app; simpl; auto.
-  - apply IHKs in HKe.
-    destruct HKe as (Ks' & e' & -> & He' & HKs').
+  - apply IHKs in H2.
+    destruct H2 as (Ks' & e' & -> & He' & HKs').
     exists (Ks' ++ [SkipCtx]), e'.
     rewrite fill_app /=; repeat split; trivial.
     apply same_skel_list_ectx_app; simpl; auto.
@@ -148,7 +84,7 @@ Proof.
     intros (Ks'' & Ks3 & HKs &
             (z & ? & ? & ->%Forall2_nil_inv_l & ->)%Forall2_cons_inv_l & ->)
            %List.Forall2_app_inv_l; simpl in *;
-      destruct z; try done; rewrite fill_app /=; auto.
+      destruct z; subst; try done; rewrite fill_app /=; auto.
 Qed.
 
 (* Generalize over all the syntax, and same_skeleton environments. I proved
@@ -159,7 +95,8 @@ Lemma same_skel_vl_subst e e' v v':
 Proof.
   move: e'; induction e; destruct e';
   move => Hske Hskv;
-    cbn in Hske |- *; try inversion Hske; ev; asimpl; auto.
+    cbn; inversion Hske; ev; cbn in *; subst; auto.
+  (* asimpl. *)
 Admitted.
 
 (* Just a test proof. *)
@@ -168,9 +105,9 @@ Lemma same_skel_tm_subst e e' v v':
   same_skel_tm (e.|[v/]) (e'.|[v'/]).
 Proof.
   move: e'; induction e; destruct e';
-  move => Hske Hskv;
-    cbn in Hske |- *; try inversion Hske; ev; asimpl; auto using same_skel_vl_subst.
-  Qed.
+  move => Hske Hskv /=;
+    inversion Hske; ev; auto using same_skel_vl_subst.
+Qed.
 
 (* Maybe copy-paste instead same_skel_dms from above. Or switch everything to an inductive definition, *)
 Definition same_skel_dms (ds1 ds2: dms): Prop :=
@@ -189,18 +126,40 @@ Ltac destruct_matches :=
                   | H: ?A ∧ ?B |- _ => destruct H
                   end.
 
+(** Apply [tac] to any [same_skel_*] hypothesis. *)
+Ltac with_same_skel tac :=
+  match goal with
+    | H: same_skel_ty _ _ |- _ => tac H
+    | H: same_skel_tm _ _ |- _ => tac H
+    | H: same_skel_vl _ _ |- _ => tac H
+    | H: same_skel_dm _ _ |- _ => tac H
+    | H: same_skel_path _ _ |- _ => tac H
+  end.
+
+(** Loop applying [tac] to all [same_skel_*] hypotheses,
+    but once per each.
+  *)
+Ltac with_same_skel_loop tac :=
+  repeat progress with_same_skel
+    ltac:(fun H => try_once_tac H (tac H));
+  un_usedLemma.
+
 Lemma simulation_skeleton_head t1' t1 t2 σ σ' ts:
   same_skel_tm t1 t1' →
   head_step t1 σ [] t2 σ' ts →
   exists t2', head_step t1' σ [] t2' σ' ts ∧ same_skel_tm t2 t2'.
 Proof.
-  move=> Hsk Hhs. inversion Hhs; subst; simpl in *.
-  - destruct_matches. eexists. split; first by econstructor. by eapply same_skel_tm_subst.
-  - destruct_matches. subst. destruct (same_skel_dms_index ds l1 v l0) as [? [? ?]]; try done.
-    + clear Hhs H. red; generalize dependent l1; induction ds; destruct l1; try constructor; ev; try apply IHds; done.
-    + eexists.
-    split; [by econstructor | done].
-  - destruct_matches. eexists. split; by [econstructor|idtac].
+  move=> Hsk Hhs. inversion Hhs; subst; with_same_skel_loop ltac:(fun P => nosplit (inverse P)).
+  - eexists. split; first by econstructor. by eapply same_skel_tm_subst.
+  - destruct (same_skel_dms_index ds ds2 v l) as [? [? ?]]; try done.
+    + clear Hhs H. hnf.
+      (* Rewrite all hyps that mention ds. *)
+      generalize dependent ds => ds.
+      rewrite !Forall2_fmap=>*.
+      decompose_Forall.
+      repeat case_match; intuition.
+    + by eexists; split; econstructor.
+  - eexists. split; by [econstructor|].
 Qed.
 
 Theorem simulation_skeleton t1 t1' t2 σ σ' ts:
