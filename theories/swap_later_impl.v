a34d23526fd3776c3beaeb0019a42a61603e73c4
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-05-09 20:52:17 +0200

    Generalize to uPred

--- theories/swap_later_impl.v
+++ theories/swap_later_impl.v
@@ -13,16 +13,16 @@ Lemma impl_laterN n `{SwapProp PROP} (P Q: PROP) : (▷^n P → ▷^n Q) ⊢ ▷
 Proof. elim: n => [//|n IHn]. by rewrite impl_later IHn. Qed.
 
 Section derived_swap_lemmas.
-  Context `{Σ: gFunctors}.
-  Lemma mlater_pers (P: iProp Σ) : □ ▷ P ⊣⊢ ▷ □ P.
+  Context `{M : ucmraT}.
+  Lemma mlater_pers (P: uPred M) : □ ▷ P ⊣⊢ ▷ □ P.
   Proof. iSplit; by iIntros "#? !>!>". Qed.
-  Lemma mlaterN_pers (P: iProp Σ) i : □ ▷^i P ⊣⊢ ▷^i □ P.
+  Lemma mlaterN_pers (P: uPred M) i : □ ▷^i P ⊣⊢ ▷^i □ P.
   Proof. iSplit; by iIntros "#? !>!>". Qed.
 
-  Context `{!SwapProp (iPropSI Σ)}.
-  Lemma mlater_impl (P Q: iProp Σ) : (▷ P → ▷ Q) ⊣⊢ ▷ (P → Q).
+  Context `{!SwapProp (uPredSI M)}.
+  Lemma mlater_impl (P Q: uPred M) : (▷ P → ▷ Q) ⊣⊢ ▷ (P → Q).
   Proof. iSplit. iApply impl_later. iApply later_impl. Qed.
-  Lemma mlaterN_impl (P Q: iProp Σ) i : (▷^i P → ▷^i Q) ⊣⊢ ▷^i (P → Q).
+  Lemma mlaterN_impl (P Q: uPred M) i : (▷^i P → ▷^i Q) ⊣⊢ ▷^i (P → Q).
   Proof. iSplit. iApply impl_laterN. iApply laterN_impl. Qed.
 End derived_swap_lemmas.
 
