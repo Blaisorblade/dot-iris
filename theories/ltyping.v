64d16930a9595f48f25942dda7a6fc376fbfb5d2
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-05-10 15:47:50 +0200

    Lemmas moved away that can't live here

--- theories/ltyping.v
+++ theories/ltyping.v
@@ -618,3 +618,116 @@ Section sort_lemmas_2.
   Qed.
 End sort_lemmas_2.
 Hint Resolve nclosed_idsσ nclosed_σ_to_subst @subst_compose_x @subst_compose_idsσ_x.
+
+(* (** The following ones are "direct" lemmas: deduce that an expression is closed *)
+(*     by knowing that its subexpression are closed. *) *)
+
+(* Lemma fv_tskip e n: nclosed e n → nclosed (tskip e) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_tproj e l n: nclosed e n → nclosed (tproj e l) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_tapp e1 e2 n: nclosed e1 n → nclosed e2 n → nclosed (tapp e1 e2) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_tv v n: nclosed_vl v n → nclosed (tv v) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_vobj ds n: nclosed ds (S n) → nclosed_vl (vobj ds) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_vabs e n: nclosed e (S n) → nclosed_vl (vabs e) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_dvl v n: nclosed_vl v n → nclosed (dvl v) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_dtysyn T n: nclosed T n → nclosed (dtysyn T) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_pv v n: nclosed_vl v n → nclosed (pv v) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_pself p l n: nclosed p n → nclosed (pself p l) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TAnd T1 T2 n: nclosed T1 n → *)
+(*                        nclosed T2 n → *)
+(*                        nclosed (TAnd T1 T2) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TOr T1 T2 n: nclosed T1 n → *)
+(*                        nclosed T2 n → *)
+(*                        nclosed (TOr T1 T2) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TMu T1 n: nclosed T1 (S n) → nclosed (TMu T1) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TLater T n: nclosed T n → nclosed (TLater T) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TAll T1 T2 n: nclosed T1 n → *)
+(*                        nclosed T2 (S n) → *)
+(*                        nclosed (TAll T1 T2) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TVMem l T1 n: nclosed T1 n → *)
+(*                        nclosed (TVMem l T1) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TTMem l T1 T2 n: nclosed T1 n → *)
+(*                         nclosed T2 n → *)
+(*                         nclosed (TTMem l T1 T2) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_TSel p l n: nclosed p n → nclosed (TSel p l) n. *)
+(* Proof. solve_fv_congruence. Qed. *)
+
+
+(* Lemma fv_dtysem σ s n: nclosed_σ σ n → nclosed (dtysem σ s) n. *)
+(* Proof. move => /Forall_to_closed_vls. solve_fv_congruence. Qed. *)
+
+(* Lemma fv_dtysem_inv σ s n: nclosed (dtysem σ s) n → nclosed_σ σ n. *)
+(* Proof. intro. apply closed_vls_to_Forall. eauto with fv. Qed. *)
+
+
+(* Lemma fv_tv_inv v n: nclosed (tv v) n → nclosed_vl v n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_tapp_inv_1 n e1 e2: nclosed (tapp e1 e2) n → nclosed e1 n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_tapp_inv_2 n e1 e2: nclosed (tapp e1 e2) n → nclosed e2 n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_tskip_inv t n: nclosed (tskip t) n → nclosed t n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_vabs_inv e n: nclosed_vl (vabs e) n → nclosed e (S n). *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_vobj_inv ds n: nclosed_vl (vobj ds) n → nclosed_ds ds (S n). *)
+(* Proof. intro. apply closed_dms_to_Forall. solve_inv_fv_congruence_h H. Qed. *)
+
+(* Lemma fv_pv_inv v n: nclosed (pv v) n → nclosed_vl v n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_TMu_inv T1 n: nclosed (TMu T1) n → nclosed T1 (S n). *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_TAll_inv_1 n T1 T2: nclosed (TAll T1 T2) n → nclosed T1 n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_TAll_inv_2 n T1 T2: nclosed (TAll T1 T2) n → nclosed T2 (S n). *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_TTMem_inv_1 n l T1 T2: nclosed (TTMem l T1 T2) n → nclosed T1 n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_TTMem_inv_2 n l T1 T2: nclosed (TTMem l T1 T2) n → nclosed T2 n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
+
+(* Lemma fv_TSel_inv p l n: nclosed (TSel p l) n → nclosed p n. *)
+(* Proof. solve_inv_fv_congruence. Qed. *)
