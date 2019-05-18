2a295584da6f807e48f0c06ade54d22fcdcb7aea
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-05-18 21:35:27 +0200

    Move generic code from closed_subst to genSynLemmas

--- theories/Dot/genSynLemmas.v
+++ theories/Dot/genSynLemmas.v
@@ -277,3 +277,71 @@ Proof.
   apply Hclt1.
   move => [|x] Hxn //=; auto with lia.
 Qed.
+
+(*
+  TODO: part of this code implements a variant of lemmas above, but for
+  arbitrary substitutions, not just the ones produced
+  by to_subst. Reducing the overlap might be good.
+ *)
+Set Implicit Arguments.
+Definition nclosed_sub n m s :=
+  ∀ i, i < n → nclosed_vl (s i) m.
+Definition nclosed_ren n m (r: var → var) := nclosed_sub n m (ren r).
+
+Lemma compose_sub_closed s s1 s2 i j:
+  nclosed_sub i j s → eq_n_s s1 s2 j → eq_n_s (s >> s1) (s >> s2) i.
+Proof. move => /= Hs Heqs x Hxi. exact: Hs. Qed.
+
+Lemma nclosed_ren_shift n m j:
+  m >= j + n → nclosed_ren n m (+j).
+Proof. move=>???/=; eauto with lia. Qed.
+Hint Resolve nclosed_ren_shift.
+
+Lemma nclosed_sub_vl v s i j:
+  nclosed_sub i j s →
+  nclosed_vl v i → nclosed_vl v.[s] j.
+Proof. move => Hcls Hclv s1 s2 Heqs; asimpl. by eapply Hclv, compose_sub_closed. Qed.
+
+Lemma nclosed_sub_x `{Ids A} `{HSubst vl A} {hsla: HSubstLemmas vl A} (a: A) s i j:
+  nclosed_sub i j s →
+  nclosed a i → nclosed a.|[s] j.
+Proof. move => Hcls Hcla s1 s2 Heqs; asimpl. by eapply Hcla, compose_sub_closed. Qed.
+
+Lemma nclosed_ren_vl v r i j:
+  nclosed_ren i j r →
+  nclosed_vl v i → nclosed_vl (rename r v) j.
+Proof. asimpl; exact: nclosed_sub_vl. Qed.
+
+Lemma nclosed_sub_up i j s:
+  nclosed_sub i j s →
+  nclosed_sub (S i) (S j) (up s).
+Proof.
+  move => //= Hs [|x] Hx. by eauto with lia.
+  asimpl; rewrite -rename_subst.
+  eapply nclosed_ren_vl; by eauto with lia.
+Qed.
+Hint Resolve nclosed_sub_up.
+
+Lemma nclosed_ren_up n m r:
+  nclosed_ren n m r →
+  nclosed_ren (S n) (S m) (upren r).
+Proof. move => //= Hr [|i] Hi; asimpl; eauto with lia. Qed.
+Hint Resolve nclosed_ren_up.
+
+Lemma nclosed_sub_inv_var x w i j k: j + k <= i →
+  nclosed_vl (ids x).[upn j (w .: ids) >> ren (+k)] i →
+  nclosed_vl (ids x) (S i).
+Proof.
+  rewrite /= !nclosed_vl_ids_equiv iter_up.
+  case: (lt_dec x j) => [?|Hge]; first lia.
+  case: (decide (x = j)) => [->|Hne]; first lia.
+  case (x - j) as [|xj] eqn:Hxj; first lia.
+  rewrite nclosed_vl_ids_equiv /=. lia.
+Qed.
+
+Lemma nclosed_ren_rev_var i j k x:
+  nclosed_vl (ids x).[upn k (ren (+j))] (i + j + k) → nclosed_vl (ids x) (i + k).
+Proof.
+  rewrite /= !nclosed_vl_ids_equiv iter_up.
+  case_match; rewrite /= nclosed_vl_ids_equiv /=; omega.
+Qed.
