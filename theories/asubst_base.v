744ec92aa0021ba5a1b1c6c9b3e0c0b422a3891f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-05-25 15:03:31 +0200

    asubst_base.v: French-style spacing before colons

--- theories/asubst_base.v
+++ theories/asubst_base.v
@@ -1,39 +1,39 @@
 From D Require Import prelude.
 
 Module Type Values.
-  Parameter vl: Type.
+  Parameter vl : Type.
   Definition vls := list vl.
-  Declare Instance inh_vl: Inhabited vl.
-  Declare Instance ids_vl: Ids vl.
-  Declare Instance inj_ids: Inj (=) (=@{vl}) ids.
+  Declare Instance inh_vl : Inhabited vl.
+  Declare Instance ids_vl : Ids vl.
+  Declare Instance inj_ids : Inj (=) (=@{vl}) ids.
 
-  Declare Instance rename_vl: Rename vl.
-  Declare Instance subst_vl: Subst vl.
-  Declare Instance subst_lemmas_vl: SubstLemmas vl.
+  Declare Instance rename_vl : Rename vl.
+  Declare Instance subst_vl : Subst vl.
+  Declare Instance subst_lemmas_vl : SubstLemmas vl.
 End Values.
 
-Module Sorts (values: Values).
+Module Sorts (values : Values).
   Import values.
 
-  Class Sort (s: Type)
-    {inh_s: Inhabited s}
-    {ids_s: Ids s} {ren_s: Rename s} {hsubst_vl_s: HSubst vl s}
-    {hsubst_lemmas_vl_s: HSubstLemmas vl s} := {}.
+  Class Sort (s : Type)
+    {inh_s : Inhabited s}
+    {ids_s : Ids s} {ren_s : Rename s} {hsubst_vl_s : HSubst vl s}
+    {hsubst_lemmas_vl_s : HSubstLemmas vl s} := {}.
 
-  Global Instance sort_vls: Sort vls := {}.
-  Global Instance sort_list `{Sort X}: Sort (list X) := {}.
-  Global Instance sort_pair_snd `{Sort X} `{Inhabited A}: Sort (A * X) := {}.
-  Global Instance sort_list_pair_snd `{Sort X} `{Inhabited A}: Sort (list (A * X)) := {}.
+  Global Instance sort_vls : Sort vls := {}.
+  Global Instance sort_list `{Sort X} : Sort (list X) := {}.
+  Global Instance sort_pair_snd `{Sort X} `{Inhabited A} : Sort (A * X) := {}.
+  Global Instance sort_list_pair_snd `{Sort X} `{Inhabited A} : Sort (list (A * X)) := {}.
 
-  Implicit Types (v: vl) (vs: vls).
+  Implicit Types (v : vl) (vs : vls).
 
-  Definition eq_n_s (s1 s2: var → vl) n := ∀ x, x < n → s1 x = s2 x.
+  Definition eq_n_s (s1 s2 : var → vl) n := ∀ x, x < n → s1 x = s2 x.
   Global Arguments eq_n_s /.
 
-  Definition to_subst (ρ: vls) : var → vl := foldr (λ v s, v .: s) ids ρ.
-  Definition subst_sigma (σ: vls) (ρ: vls) := σ.|[to_subst ρ].
+  Definition to_subst (ρ : vls) : var → vl := foldr (λ v s, v .: s) ids ρ.
+  Definition subst_sigma (σ : vls) (ρ : vls) := σ.|[to_subst ρ].
 
-  Lemma to_subst_nil: to_subst [] = ids.
+  Lemma to_subst_nil : to_subst [] = ids.
   Proof. trivial. Qed.
 
   Lemma to_subst_cons v ρ : to_subst (v :: ρ) = v .: to_subst ρ.
@@ -43,11 +43,11 @@ Module Sorts (values: Values).
   Global Typeclasses Opaque to_subst.
   Global Opaque to_subst.
 
-  Definition push_var (σ: vls) : vls := ids 0 :: σ.|[ren (+1)].
+  Definition push_var (σ : vls) : vls := ids 0 :: σ.|[ren (+1)].
   Arguments push_var /.
 
   (** Create an identity environment of the given length. *)
-  Fixpoint idsσ n: vls :=
+  Fixpoint idsσ n : vls :=
     match n with
     | 0 => []
     | S n => push_var (idsσ n)
@@ -56,10 +56,10 @@ Module Sorts (values: Values).
   (** [n]-closedness defines when some AST has at most [n] free variables (from [0] to [n - 1]). *)
   (** Here and elsewhere, we give one definition for values, using [subst], and
       another for other ASTs, using [hsubst]. *)
-  Definition nclosed_vl (v: vl) n :=
+  Definition nclosed_vl (v : vl) n :=
     ∀ s1 s2, eq_n_s s1 s2 n → v.[s1] = v.[s2].
 
-  Definition nclosed `{HSubst vl X} (x: X) n :=
+  Definition nclosed `{HSubst vl X} (x : X) n :=
     ∀ s1 s2, eq_n_s s1 s2 n → x.|[s1] = x.|[s2].
 
   Definition nclosed_σ σ n := Forall (λ v, nclosed_vl v n) σ.
@@ -69,7 +69,7 @@ Module Sorts (values: Values).
       by knowing that its subexpression are closed. *)
 
   (** Needed by solve_fv_congruence when dealing with binders, such as in fv_vobj and fv_vabs. *)
-  Lemma eq_up s1 s2 n: eq_n_s s1 s2 n → eq_n_s (up s1) (up s2) (S n).
+  Lemma eq_up s1 s2 n : eq_n_s s1 s2 n → eq_n_s (up s1) (up s2) (S n).
   Proof.
     rewrite /up. move => Heq [|x] Hl //=. f_equiv. apply Heq. lia.
   Qed.
@@ -78,13 +78,13 @@ Module Sorts (values: Values).
     rewrite /nclosed /nclosed_vl => * /=; repeat (f_equiv; try solve [(idtac + asimpl); auto using eq_up]).
 
   (** Generic direct lemmas. *)
-  Lemma fv_cons `{Sort X} (x: X) xs n: nclosed xs n → nclosed x n → nclosed (x :: xs) n.
+  Lemma fv_cons `{Sort X} (x : X) xs n : nclosed xs n → nclosed x n → nclosed (x :: xs) n.
   Proof. solve_fv_congruence. Qed.
 
-  Lemma fv_pair `{Sort X} `{Inhabited A} (a: A) (x: X) n: nclosed x n → nclosed (a, x) n.
+  Lemma fv_pair `{Sort X} `{Inhabited A} (a : A) (x : X) n : nclosed x n → nclosed (a, x) n.
   Proof. solve_fv_congruence. Qed.
 
-  Lemma fv_pair_cons `{Sort X} `{!Inhabited A} (a: A) (x: X) xs n: nclosed xs n → nclosed x n → nclosed ((a, x) :: xs) n.
+  Lemma fv_pair_cons `{Sort X} `{!Inhabited A} (a : A) (x : X) xs n : nclosed xs n → nclosed x n → nclosed ((a, x) :: xs) n.
   (* solve_fv_congruence would work, but this gives a smaller proof. *)
   Proof. intros. by apply fv_cons, fv_pair. Qed.
 
@@ -94,19 +94,19 @@ Module Sorts (values: Values).
       See fv_vabs_inv_manual for an explanation. *)
 
   Definition stail s := ren (+1) >> s.
-  Definition shead (s: var → vl) := s 0.
+  Definition shead (s : var → vl) := s 0.
 
-  Lemma eq_n_s_tails {n s1 s2}: eq_n_s s1 s2 (S n) → eq_n_s (stail s1) (stail s2) n.
+  Lemma eq_n_s_tails {n s1 s2} : eq_n_s s1 s2 (S n) → eq_n_s (stail s1) (stail s2) n.
   Proof.
     move => /= HsEq x Hl.
     rewrite /stail /= !id_subst.
     apply HsEq. lia.
   Qed.
 
-  Lemma eq_n_s_heads {n s1 s2}: eq_n_s s1 s2 n → n > 0 → shead s1 = shead s2.
+  Lemma eq_n_s_heads {n s1 s2} : eq_n_s s1 s2 n → n > 0 → shead s1 = shead s2.
   Proof. rewrite /shead => /= HsEq. exact: HsEq. Qed.
 
-  Lemma decomp_s_vl v s: v.[s] = v.[up (stail s)].[shead s/].
+  Lemma decomp_s_vl v s : v.[s] = v.[up (stail s)].[shead s/].
   Proof. by rewrite /stail /shead; asimpl. Qed.
 
   Notation cl_ρ ρ := (nclosed_σ ρ 0).
@@ -114,7 +114,7 @@ Module Sorts (values: Values).
   Section sort.
     Context `{Sort X}.
 
-    Lemma decomp_s (x: X) s:
+    Lemma decomp_s (x : X) s :
       x.|[s] = x.|[up (stail s)].|[shead s/].
     Proof. rewrite /stail /shead. by asimpl. Qed.
   End sort.
@@ -143,11 +143,11 @@ Module Sorts (values: Values).
 
   Ltac solve_inv_fv_congruence_auto :=
     match goal with
-    | Hcl: nclosed ?x ?n |- nclosed _ _ => solve_inv_fv_congruence_h Hcl
-    | Hcl: nclosed_vl ?v ?n |- nclosed _ _ => solve_inv_fv_congruence_h Hcl
-    | Hcl: nclosed ?x ?n |- nclosed_vl _ _ => solve_inv_fv_congruence_h Hcl
-    | Hcl: nclosed_vl ?v ?n |- nclosed_vl _ _ => solve_inv_fv_congruence_h Hcl
+    | Hcl : nclosed ?x ?n |- nclosed _ _ => solve_inv_fv_congruence_h Hcl
+    | Hcl : nclosed_vl ?v ?n |- nclosed _ _ => solve_inv_fv_congruence_h Hcl
+    | Hcl : nclosed ?x ?n |- nclosed_vl _ _ => solve_inv_fv_congruence_h Hcl
+    | Hcl : nclosed_vl ?v ?n |- nclosed_vl _ _ => solve_inv_fv_congruence_h Hcl
     end.
 
-  Hint Extern 10 => solve_inv_fv_congruence_auto: fv.
+  Hint Extern 10 => solve_inv_fv_congruence_auto : fv.
 End Sorts.
