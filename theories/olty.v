fa238de4fe21fa80fe78efcd6dbd27da994708ae
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-05-29 23:12:43 +0200

    More WIP, example lemma on and.

--- theories/olty.v
+++ theories/olty.v
@@ -102,25 +102,79 @@ Definition env_typed (Γ : ctx) : vls -c> iProp Σ := env_oltyped (ty_interp_env
 
 Instance env_typed_persistent `{TyInterp ty Σ} Γ ρ : Persistent (env_typed Γ ρ) := env_oltyped_persistent _ _.
 
-Definition judgment Σ s : Type := s * (vls -c> s -c> iProp Σ).
+Definition judgment Σ s : Type := option s * (vls -c> option s -c> iProp Σ).
+Definition nosubj_judgment Σ : Type := vls -c> iProp Σ.
+Definition subj_judgment Σ s : Type := s * (vls -c> s -c> iProp Σ).
+Program Definition subj_judgment_to_judgment {Σ s} : subj_judgment Σ s → judgment Σ s :=
+  λ '(x, φ), (Some x, λ ρ, from_option (φ ρ) False)%I.
 
-Definition judgment_holds `{Closeable s} (Γ : sCtx) (J : judgment Σ s) :=
-  (⌜ nclosed_s (fst J) (length Γ) ⌝ ∗ □∀ ρ, env_oltyped Γ ρ → (snd J) ρ (fst J))%I.
+Definition judgment_holds `{Closeable s} (Γ : sCtx) (J : judgment Σ s): iProp Σ :=
+  (⌜ from_option (flip nclosed_s (length Γ)) True (fst J) ⌝ ∗ □∀ ρ, env_oltyped Γ ρ → (snd J) ρ (fst J))%I.
 Notation "Γ ⊨ J" := (judgment_holds Γ J) (at level 74, J at next level).
 Global Arguments judgment_holds /.
 
-Definition ivtp (φ: olty Σ) v : judgment Σ vl := (v, olty_car φ).
+Program Definition ivtp (φ: olty Σ) v : judgment Σ vl := subj_judgment_to_judgment (v, φ).
 Global Arguments ivtp /.
 
 (* DOT/D<: judgments are indexed by [⋅]. *)
 Notation "v v⋅: φ" := (ivtp φ v) (at level 73).
 Definition judge_me Γ v φ := Γ ⊨ v v⋅: φ.
 (* About judge_me. *)
-  (* Context {Λ: language}.
-  Fail Definition interp_expr (φ : olty Σ) :=
-    λ ρ t, WP t {{ φ ρ }} %I. *)
-  (* Global Arguments interp_expr /. *)
+  (* Context {Λ: language}. *)
+From D.pure_program_logic Require Import lifting.
+From iris.program_logic Require Import ectxi_language ectx_language.
+From D Require Export gen_iheap saved_interp.
 
+Class dlangG Σ := DLangG {
+  dlangG_savior :> savedInterpG Σ vls vl;
+  dlangG_interpNames : gen_iheapG stamp gname Σ;
+}.
+
+Class dlangPreG Σ := DLangPreG {
+  dlangPreG_savior :> savedInterpG Σ vls vl;
+  dlangPreG_interpNames : gen_iheapPreG stamp gname Σ;
+}.
+
+Instance dlangG_irisG `{dlangG Σ} : irisG Λ Σ := {
+  state_interp σ κs _ := True%I;
+  fork_post _ := True%I;
+}.
+Context `{dlangG Σ}.
+
+Definition interp_expr (φ : olty Σ) :=
+  λ ρ t, WP t {{ φ ρ }} %I.
+Global Arguments interp_expr /.
+Definition tm := expr Λ.
+Context `{Closeable tm}.
+Definition ittp (φ: olty Σ) t : judgment Σ tm := subj_judgment_to_judgment (t, interp_expr φ).
+Global Arguments ittp /.
+
+(* DOT/D<: judgments are indexed by [⋅]. *)
+Notation "t t⋅: φ" := (ittp φ t) (at level 73).
+Definition judge_me2 Γ t φ := Γ ⊨ t t⋅: φ.
+About judge_me2.
+(* Choosing vl is arbitrary. *)
+Program Definition nosubj_judgment_to_judgment {Σ} : nosubj_judgment Σ → judgment Σ vl :=
+  λ φ, (None, λ ρ _, φ ρ)%I.
+Implicit Type (φ: olty Σ).
+
+Definition ivstp φ1 φ2 : nosubj_judgment Σ := (λ ρ, ∀ v, ⌜ nclosed_vl v 0 ⌝ → φ1 ρ v → φ2 ρ v)%I.
+Program Definition step_indexed_ivstp φ1 i1 φ2 i2 := nosubj_judgment_to_judgment (Σ := Σ)
+  (λ ρ, ∀ v, ⌜ nclosed_vl v 0 ⌝ → (▷^i1 φ1 ρ v) → ▷^i2 φ2 ρ v)%I.
+Notation "[ φ1 , i1 ] <: [ φ2 , i2 ]" := (step_indexed_ivstp φ1 i1 φ2 i2) (at level 73).
+From iris.proofmode Require Import tactics.
+Lemma equiv_vstp Γ (φ1 φ2: olty Σ) i1 i2: (Γ ⊨ [φ1 , i1] <: [φ2 , i2]) ⊣⊢
+    (□∀ ρ v, ⌜ nclosed_vl v 0 ⌝ → env_oltyped Γ ρ → (▷^i1 φ1 ρ v) → ▷^i2 φ2 ρ v)%I.
+Proof.
+  iSplit; [iIntros "#[_ H] /= !>" (???) "#?" |
+    iIntros "#H"; iSplit; first done; iIntros "!>" (?) "#? /="; iIntros (??)].
+  all: by iApply "H".
+Qed.
+Definition oAnd φ1 φ2 : olty Σ := Olty (λ ρ v, φ1 ρ v ∧ φ2 ρ v)%I.
+Lemma andstp1 Γ φ1 φ2 i : (Γ ⊨ [oAnd φ1 φ2 , i] <: [φ1 , i]).
+Proof.
+  rewrite equiv_vstp /=. by iIntros "!>" (???) "#Hg #[? ?]".
+Qed.
 End judgments.
 (* Module Type Foo.
 Parameter vlL : language. with vlL.vl := vl. *)
