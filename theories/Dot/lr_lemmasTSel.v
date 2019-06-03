add11a59a981f8e598ce906d2c54c01a5f382272
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-06-04 01:58:41 +0200

    Support lemmas for paths, and refactorings
    Reuse some lemmas from step_fv.

--- theories/Dot/lr_lemmasTSel.v
+++ theories/Dot/lr_lemmasTSel.v
@@ -7,21 +7,6 @@ Implicit Types (L T U: ty) (v: vl) (e: tm) (d: dm) (ds: dms) (Γ : list ty).
 Section Sec.
   Context `{HdlangG: dlangG Σ} Γ.
 
-  (* Next step: proper lemma on arbitrary-length paths. *)
-  Lemma mem_stp_sel_sub_path1 L U va l1 l2:
-    (ivtp Γ (TVMem l1 (TTMem l2 L U)) va -∗
-    ivstp Γ (TLater L) ((TSel (pself (pv va) l1) l2)))%I.
-  Proof.
-    iIntros "/= #[% Hva] !>" (ρ v) "#Hg #[% HvL]". move: H H0 => Hclva Hclv. iFrame (Hclv).
-    iDestruct ("Hva" $! ρ with "Hg") as (Hclvas d Hl Hcld vmem) "#[-> #[_ H1]]"; iClear "Hva".
-    iDestruct "H1" as (d1) "[Hl2 [_ H]]".
-    iDestruct "H" as (φ) "#[Hl [#HLφ #HφU]]".
-    iDestruct ("HLφ" $! _ Hclv with "HvL") as "HLφ'".
-    iExists vmem.
-    iSplit => //.
-    iExists φ, d1. repeat iModIntro; by repeat iSplit.
-  Qed.
-
   Lemma Sub_Sel L U va l i:
     Γ ⊨ tv va : TTMem l L U, i -∗
     Γ ⊨ [TLater L, i] <: [TSel (pv va) l, i].
