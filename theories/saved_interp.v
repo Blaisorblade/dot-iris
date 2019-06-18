f248feca26eaac7066674b861d1d6e21bf1a2c55
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-06-18 15:52:47 +0200

    Saved_interp: nits

--- theories/saved_interp.v
+++ theories/saved_interp.v
@@ -1,23 +1,19 @@
-From iris.algebra Require Import ofe agree.
 From iris.proofmode Require Import tactics.
-From iris.base_logic Require Import lib.iprop (* For gname *)
-     lib.saved_prop.
-From iris.program_logic Require Import weakestpre.
-
-From D Require Import tactics.
+From iris.base_logic Require Import lib.saved_prop.
 
 (** saved interpretations *)
 
 Notation savedInterpG Σ A B := (savedAnythingG Σ (A -c> B -c> ▶ ∙)).
 Notation savedInterpΣ A B := (savedAnythingΣ (A -c> B -c> ▶ ∙)).
+
 Section saved_interp.
-  Context {vls vl: Type}.
-  Context `{!savedInterpG Σ vls vl}.
-  Implicit Type (Φ : vls -c> vl -c> iProp Σ).
+  Context {A B: Type}.
+  Context `{!savedInterpG Σ A B}.
+  Implicit Type (Φ : A -c> B -c> iProp Σ).
 
   Definition saved_interp_own (γ : gname) Φ :=
     saved_anything_own
-      (F := vls -c> vl -c> ▶ ∙) γ (λ vs v, Next (Φ vs v)).
+      (F := A -c> B -c> ▶ ∙) γ (λ a b, Next (Φ a b)).
 
   Instance saved_interp_own_contractive γ : Contractive (saved_interp_own γ).
   Proof.
@@ -27,27 +23,20 @@ Section saved_interp.
 
   Lemma saved_interp_alloc_strong (G : gset gname) Φ :
     (|==> ∃ γ, ⌜γ ∉ G⌝ ∧ saved_interp_own γ Φ)%I.
-  Proof. iApply saved_anything_alloc_cofinite. Qed.
+  Proof. apply saved_anything_alloc_cofinite. Qed.
 
   Lemma saved_interp_alloc Φ :
     (|==> ∃ γ, saved_interp_own γ Φ)%I.
-  Proof. iApply saved_anything_alloc. Qed.
+  Proof. apply saved_anything_alloc. Qed.
 
-  Lemma saved_interp_agree γ Φ Ψ vs v :
-    saved_interp_own γ Φ -∗ saved_interp_own γ Ψ -∗ ▷ (Φ vs v ≡ Ψ vs v).
+  Lemma saved_interp_agree γ Φ Ψ a b :
+    saved_interp_own γ Φ -∗ saved_interp_own γ Ψ -∗
+    ▷ (Φ a b ≡ Ψ a b).
   Proof.
-    iIntros "#HΦ #HΨ /=". iApply bi.later_equivI.
+    iIntros "HΦ HΨ /=".
     iDestruct (saved_anything_agree with "HΦ HΨ") as "Heq".
-    rewrite bi.ofe_fun_equivI; iSpecialize ("Heq" $! vs).
-    by rewrite bi.ofe_fun_equivI; iSpecialize ("Heq" $! v).
-  Qed.
-
-  Lemma saved_interp_impl γ Φ Ψ vs v :
-    saved_interp_own γ Φ -∗ saved_interp_own γ Ψ -∗ □ (▷ Φ vs v -∗ ▷ Ψ vs v).
-  Proof.
-    iIntros "#HΦ #HΨ /= !# H1".
-    iPoseProof (saved_interp_agree _ _ _ vs v with "HΦ HΨ") as "Heq".
-    by iNext; iRewrite -"Heq".
+    repeat setoid_rewrite bi.ofe_fun_equivI.
+    iApply bi.later_equivI; iApply "Heq".
   Qed.
 End saved_interp.
 
