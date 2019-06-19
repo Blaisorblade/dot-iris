8fc5f01906ed24d98e514064d850ff690cf75f66
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-06-19 18:14:29 +0200

    Reimplement saved_interp by currying

--- theories/saved_interp.v
+++ theories/saved_interp.v
@@ -4,20 +4,24 @@ Require Import D.iris_prelude.
 
 (** saved interpretations *)
 
-Notation savedInterpG Σ A B := (savedAnythingG Σ (A -c> B -c> ▶ ∙)).
-Notation savedInterpΣ A B := (savedAnythingΣ (A -c> B -c> ▶ ∙)).
+Notation savedInterpG Σ A B := (savedPredG Σ (A * B)).
+Notation savedInterpΣ A B := (savedPredΣ (A * B)).
 
 Section saved_interp.
   Context {A B : Type}.
   Context `{!savedInterpG Σ A B}.
   Implicit Type (Φ : A -c> B -c> iProp Σ).
 
+  Definition curryC Φ : A * B -c> iProp Σ := (λ '(a, b), Φ a b).
+
   Definition saved_interp_own (γ : gname) Φ :=
-    saved_anything_own
-      (F := A -c> B -c> ▶ ∙) γ (λ a b, Next (Φ a b)).
+    saved_pred_own γ (curryC Φ).
 
   Instance saved_interp_own_contractive γ : Contractive (saved_interp_own γ).
-  Proof. rewrite /saved_interp_own /saved_anything_own. solve_contractive_ho. Qed.
+  Proof.
+    rewrite /saved_interp_own => n f g Hfg.
+    apply saved_pred_own_contractive. case: n Hfg => [//|n Hfg [] //].
+  Qed.
 
   Global Instance persistent_saved_interp_own: Persistent (saved_interp_own γ Φ) := _.
 
@@ -27,12 +31,7 @@ Section saved_interp.
   Lemma saved_interp_agree γ Φ Ψ a b :
     saved_interp_own γ Φ -∗ saved_interp_own γ Ψ -∗
     ▷ (Φ a b ≡ Ψ a b).
-  Proof.
-    iIntros "HΦ HΨ /=".
-    iDestruct (saved_anything_agree with "HΦ HΨ") as "Heq".
-    repeat setoid_rewrite bi.ofe_fun_equivI.
-    iApply bi.later_equivI; iApply "Heq".
-  Qed.
+  Proof. apply (saved_pred_agree γ (curryC Φ) (curryC Ψ) (a, b)). Qed.
 End saved_interp.
 
 Global Opaque saved_interp_own.
