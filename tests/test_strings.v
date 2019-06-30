6ae29dbe869d04495ed430817052baa149ba4b9a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-06-30 16:21:04 +0200

    Add testcase for issue with `strings.length`.

---
+++ tests/test_strings.v
@@ -0,0 +1,7 @@
+From D.Dot Require Import unary_lr.
+
+Goal length [1] = 1. idtac "Dot.unary_lr". Show. Abort.
+
+From D.Dot Require Import unary_lr_binding.
+
+Goal length [1] = 1. idtac "Dot.unary_lr_binding". Show. Abort.
---
+++ tests/test_strings2.v
@@ -0,0 +1,7 @@
+From D.Dot Require Import fundamental.
+
+Goal length [1] = 1. idtac "Dot.fundamental". Show. Abort.
+
+From D.DSubSyn Require Import fundamental.
+
+Goal length [1] = 1. idtac "DSubSyn.fundamental". Show. Abort.
