Blaisorblade/dot-iris

commit b55869449338570f182640d702331e5603bee47d

Assert right away that syn.v implements `VlSortsSig`

theories/DSub/syn.v
@@ -1,7 +1,10 @@
 From D Require Export prelude.
-From D Require Import asubst_base.
+From D Require Import asubst_intf asubst_base.
 From iris.program_logic Require ectx_language ectxi_language.
 
+(** This module is included right away. Its only point is asserting explicitly
+    that it implements [VlSortsSig]. *)
+Module VlSorts <: VlSortsSig.
 
 Inductive tm : Type :=
   | tv : vl_ -> tm
@@ -298,6 +301,8 @@ Canonical Structure dlang_ectx_lang := ectxi_language.EctxLanguageOfEctxi dlang_
 Canonical Structure dlang_lang := ectx_language.LanguageOfEctx dlang_ectx_lang.
 
 Include Sorts.
+End VlSorts.
+Include VlSorts.
 
 Instance sort_tm : Sort tm := {}.
 Instance sort_ty : Sort ty := {}.

theories/Dot/syn.v
@@ -1,8 +1,11 @@
 From stdpp Require Import strings.
 From D Require Export prelude.
-From D Require Import asubst_base.
+From D Require Import asubst_intf asubst_base.
 From iris.program_logic Require ectx_language ectxi_language.
 
+(** This module is included right away. Its only point is asserting explicitly
+    that it implements [VlSortsSig]. *)
+Module VlSorts <: VlSortsSig.
 
 Definition label := string.
 
@@ -464,6 +467,8 @@ Canonical Structure dlang_ectx_lang := ectxi_language.EctxLanguageOfEctxi dlang_
 Canonical Structure dlang_lang := ectx_language.LanguageOfEctx dlang_ectx_lang.
 
 Include Sorts.
+End VlSorts.
+Include VlSorts.
 
 Instance sort_tm : Sort tm := {}.
 Instance sort_dm : Sort dm := {}.

commit 4466d47221fda9b7ff13fe462fde4968718a01a3

Fix compilation speed regression: Replace Include by Export
Fix compilation time regression from b55869449338570f182640d702331e5603bee47d.

We ended up with an expensive hint (`solve_inv_fv_congruence_auto`) registered
twice; weirdly, it was used twice even if it *succeeded* and dispatched the
goal.

This happens because, per
https://coq.inria.fr/refman/proof-engine/tactics.html#hint-locality, Coq uses
Hints from all required modules, not just imported ones, and interactive modules
count here as required.

I tried `Global Set Loose Hint Behavior "Strict".`, but that breaks Iris.

This makes `Include` useless for modules (not for functors), but luckily we can
use `Export`.

theories/DSub/operational.v
@@ -1,7 +1,7 @@
 From D Require Import dlang.
 From D.DSub Require Export syn.
 
-Include LiftWp syn.
+Include LiftWp VlSorts.
 
 (**
 Possible future plan.

theories/DSub/syn.v
@@ -305,7 +305,7 @@ Proof. done. Qed.
 
 Include Sorts.
 End VlSorts.
-Include VlSorts.
+Export VlSorts.
 
 Instance sort_ty : Sort ty := {}.
 

theories/Dot/operational.v
@@ -1,4 +1,4 @@
 From D Require Import dlang.
 From D.Dot Require Export syn.
 
-Include LiftWp syn.
+Include LiftWp VlSorts.

theories/Dot/syn.v
@@ -471,7 +471,7 @@ Proof. done. Qed.
 
 Include Sorts.
 End VlSorts.
-Include VlSorts.
+Export VlSorts.
 
 Instance sort_dm : Sort dm := {}.
 Instance sort_path : Sort path := {}.

theories/hoInterps_experiments.v
@@ -144,7 +144,7 @@ End HoSemTypes.
 Import mapsto.
 Notation "s ↝[ n ] φ" := (∃ γ, (s ↦ γ) ∗ (γ ⤇[ n ] φ))%I  (at level 20) : bi_scope.
 
-Include HoSemTypes syn.
+Include HoSemTypes VlSorts.
 
 Section bar.
   Context `{!savedHoSemTypeG Σ} `{!dlangG Σ}.