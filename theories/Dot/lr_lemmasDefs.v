b5a12c2a5e10c83396c0bef44c5003e255ca7a8b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-09-27 03:42:08 +0200

    Nits

--- theories/Dot/lr_lemmasDefs.v
+++ theories/Dot/lr_lemmasDefs.v
@@ -106,11 +106,11 @@ Section Sec.
      Γ |L T ⊨ds ds : T -∗
      Γ ⊨ tv (vobj ds) : TMu T.
   Proof.
-    iIntros "/= #Hds !>" (vs) "#Hg /=". rewrite -wp_value'.
+    iIntros "/= #Hds !>" (ρ) "#Hg /=". rewrite -wp_value'.
     iLöb as "IH".
     iApply lift_dsinterp_dms_vl_commute.
     rewrite norm_selfSubst.
-    iApply ("Hds" $! (vobj _ .: vs)); by iFrame "IH Hg".
+    iApply ("Hds" $! (vobj _ .: ρ)); by iFrame "IH Hg".
   Qed.
 
   Lemma DNil_I : Γ ⊨ds [] : TTop.
