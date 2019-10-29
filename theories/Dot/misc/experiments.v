b7f5feacc249270642a8f11e38403eeda6e95b8d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-10-29 11:22:59 +0100

    Show stronger versions of T_Forall_I

--- theories/Dot/misc/experiments.v
+++ theories/Dot/misc/experiments.v
@@ -12,6 +12,19 @@ Implicit Types
 Section Sec.
   Context `{HdlangG: dlangG Σ}.
 
+  Lemma T_Forall_I' Γ T1 T2 e `{SwapProp (iPropSI Σ)}:
+    TLater T1.|[ren (+1)] :: Γ ⊨ e : T2 -∗
+    (*─────────────────────────*)
+    Γ ⊨ tv (vabs e) : TAll T1 T2.
+  Proof.
+    iIntros "/= #HeT !>" (vs) "#HG".
+    rewrite -wp_value'. iExists _; iSplitL; first done.
+    iIntros "!>" (v); rewrite -(decomp_s _ (v .: vs)).
+    rewrite -wand_later; iIntros "#Hv".
+    iApply ("HeT" $! (v .: vs) with "[$HG]").
+    by rewrite (interp_weaken_one T1 _ v).
+  Qed.
+
   Lemma TAll_Later_Swap0 Γ T U `{SwapProp (iPropSI Σ)}:
     Γ ⊨ TAll (TLater T) U, 0 <: TLater (TAll T U), 0.
   Proof.
@@ -28,10 +41,26 @@ Section Sec.
   Proof.
     iLöb as "IH" forall (t Φ).
     iEval (rewrite !wp_unfold /wp_pre /=).
-    case: (to_val t) => [v|]. eauto.
+    case: (to_val t) => [v|]. by eauto.
     iIntros "H" (σ1 κ κs n _); iDestruct ("H" $! σ1 κ κs n with "[#//]") as "[$ H]".
     iIntros (e2 σ2 efs Hstep); iDestruct ("H" $! e2 σ2 efs Hstep) as "[$ [H $]]".
     iApply ("IH" with "H").
+  Qed.
+
+  Lemma T_Forall_I'' Γ T1 T2 e `{SwapProp (iPropSI Σ)}:
+    TLater T1.|[ren (+1)] :: Γ ⊨ e : TLater T2 -∗
+    (*─────────────────────────*)
+    Γ ⊨ tv (vabs e) : TAll T1 T2.
+  Proof.
+    iIntros "/= #HeT !>" (vs) "#HG".
+    rewrite -wp_value'. iExists _; iSplitL; first done.
+    iIntros "!>" (v); rewrite -(decomp_s e (v .: vs)).
+    rewrite -wand_later; iIntros "#Hv".
+    (* iApply (wp_later_swap _ (⟦ T2 ⟧ (v .: vs))).
+    iApply ("HeT" $! (v .: vs) with "[$HG]"). *)
+    iSpecialize ("HeT" $! (v .: vs) with "[$HG]").
+    by rewrite (interp_weaken_one T1 _ v).
+    iApply (wp_later_swap with "HeT").
   Qed.
 
   (** Stronger version of TAll_Later_Swap0, needs wp_later_swap, which
