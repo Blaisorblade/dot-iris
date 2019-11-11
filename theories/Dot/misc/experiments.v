a99434400fa45753fb22cb3c426b45e6515bdac6
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-11-11 16:18:25 +0100

    experiments: Show how to prevent unfolding of ietp.

--- theories/Dot/misc/experiments.v
+++ theories/Dot/misc/experiments.v
@@ -8,6 +8,13 @@ From D.Dot Require Import unary_lr synLemmas rules
 Implicit Types
          (L T U: ty) (v: vl) (e: tm) (d: dm) (ds: dms) (p : path)
          (Γ : ctx).
+Section ProofModeTry.
+  Context `{HdlangG: dlangG Σ}.
+
+  (*XXX : move away from here. Avoid auto-dropping box (and unfolding) when introducing ietp persistently. *)
+  Instance: IntoPersistent false (ietp Γ T e) (ietp Γ T e) | 0 := _.
+
+End ProofModeTry.
 
 Section Sec.
   Context `{HdlangG: dlangG Σ}.
