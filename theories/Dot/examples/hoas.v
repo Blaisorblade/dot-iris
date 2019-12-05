d17689a8f157f5522f997b5281277b6095653eda
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-12-05 16:36:23 +0100

    Port list.v to use HOAS notation, and speed it up (?!?)
    As a side effect of the HOAS notation, this file now takes half the time.
    Why that happens is a mystery.

--- theories/Dot/examples/hoas.v
+++ theories/Dot/examples/hoas.v
@@ -5,15 +5,59 @@ From D Require Import tactics.
 From D.Dot Require Import syn exampleInfra.
 
 (* Inspired by the "Unembedding DSLs" paper, but specialized.
-I suspect equivalent to
+The algorithm it uses is very different from McBride's Jigger.
 https://web.archive.org/web/20130412082828/http://www.e-pig.org/epilogue/?p=773
-but readable. *)
+It’s made possible exactly because our de Bruijn terms have a type indexed by the number of variables in scope.
+*)
 (* TODO? Check out:
   http://www.cs.uu.nl/research/techreps/repo/CS-2012/2012-009.pdf
 *)
-(* Binders in our language: λ, ν, ∀, μ. *)
 Definition hterm sort := var → sort.
+Definition hclose {s1} : hterm s1 → s1 := (.$ 0).
+Global Arguments hclose /.
+Definition pureS {s1} : s1 → hterm s1 := λ x _, x.
+Global Arguments pureS /.
 
+(** Utilities to lift syntax to [hterm]s. *)
+Module Import hterm_lifting.
+Section lifting.
+Context {s1 s2 s3 s4: Type}.
+
+Definition apS : hterm (s1 → s2) → hterm s1 → hterm s2 := λ f a i, f i (a i).
+Definition bindS : hterm s1 → (s1 → hterm s2) → hterm s2 := λ m f i, f (m i) i.
+
+Definition liftA0 (con : s1) : hterm s1 := pureS con.
+Definition liftA1 (con : s1 → s2) : hterm s1 → hterm s2 := λ a1 i,
+  con (a1 i).
+
+Definition liftA2 (con : s1 → s2 → s3) :
+  hterm s1 → hterm s2 → hterm s3 := λ a1 a2 i,
+  con (a1 i) (a2 i).
+Definition liftA3 (con : s1 → s2 → s3 → s4) :
+  hterm s1 → hterm s2 → hterm s3 → hterm s4 := λ a1 a2 a3 i,
+  con (a1 i) (a2 i) (a3 i).
+
+Global Arguments apS /.
+Global Arguments bindS /.
+
+Global Arguments liftA0 /.
+Global Arguments liftA1 /.
+Global Arguments liftA2 /.
+Global Arguments liftA3 /.
+
+Definition liftBind (con : s1 → s2) (f : hterm vl → hterm s1) : hterm s2 := λ i,
+  let i' := S i in
+  let v := ren (λ j, j - i') in
+  con (f v i').
+
+Definition liftList : list (label * hterm dm) → hterm (list (label * dm)) := λ ds i, map (mapsnd (.$ i)) ds.
+
+(* Ever used? Likely not. *)
+(* Definition hshift : hterm s1 → hterm s1 := λ t i, t (S i). *)
+End lifting.
+End hterm_lifting.
+
+(* Binders in our language: λ, ν, ∀, μ. *)
 Notation htm   := (hterm tm).
 Notation hvl   := (hterm vl).
 Notation hdm   := (hterm dm).
@@ -29,103 +73,40 @@ Instance ids_hvl : Ids hvl := λ x, (* [x]: input to the substitution. *)
   (* Resulting [vl]. *)
   λ i, ids (x + i).
 
-(** Lift to the *)
-Module Import hterm_lifting.
-Section lifting.
-Context {s1 s2 s3 s4: Type}.
-Definition hclose : hterm s1 → s1 := (.$ 0).
+Global Arguments ids_hvl /.
 
-Definition pureS : s1 → hterm s1 := const.
-Definition apS : hterm (s1 → s2) → hterm s1 → hterm s2 := λ f a i, f i (a i).
-Definition bindS : hterm s1 → (s1 → hterm s2) → hterm s2 := λ m f i, f (m i) i.
+Module Import syn.
 
-Definition liftA0 (con : s1) : hterm s1 := pureS con.
-Definition liftA1 (con : s1 → s2) : hterm s1 → hterm s2 := λ a1 i,
-  con (a1 i).
-
-Definition liftA2 (con : s1 → s2 → s3) :
-  hterm s1 → hterm s2 → hterm s3 := λ a1 a2 i,
-  con (a1 i) (a2 i).
-Definition liftA3 (con : s1 → s2 → s3 → s4) :
-  hterm s1 → hterm s2 → hterm s3 → hterm s4 := λ a1 a2 a3 i,
-  con (a1 i) (a2 i) (a3 i).
-
-Definition liftBind (con : s1 → s2) (f : hterm vl → hterm s1) : hterm s2 := λ i,
-  let i' := S i in
-  let v := ren (λ j, j - i') in
-  con (f v i').
-
-Definition liftList : list (label * hterm dm) → hterm (list (label * dm)) := λ ds i, map (mapsnd (.$ i)) ds.
-
-(* Ever used? Likely not. *)
-(* Definition hshift : hterm s1 → hterm s1 := λ t i, t (S i). *)
-End lifting.
-End hterm_lifting.
-
-Module Type liftSyn.
-  Parameter htv : hvl → hterm tm.
-  Parameter htapp : hterm tm → hterm tm → hterm tm.
-  Parameter htproj : hterm tm → label → nat → tm.
-  Parameter htskip : hterm tm → hterm tm.
-
-  Parameter hvar_vl : var → hterm vl.
-  Parameter hvnat : nat → hterm vl.
-  Parameter hvabs : (hterm vl → hterm tm) → hterm vl.
-  Parameter hvobj : (hterm vl → hdms) → hterm vl.
-
-  Parameter hdtysyn : hterm ty → hterm dm.
-  Parameter hdvl : hterm vl → hterm dm.
-
-  Parameter hpv : hterm vl → hterm path.
-  Parameter hpself : hterm path → label → nat → path.
-
-  Parameter hTTop : hterm ty.
-  Parameter hTBot : hterm ty.
-  Parameter hTAnd : hterm ty → hterm ty → hterm ty.
-  Parameter hTOr : hterm ty → hterm ty → hterm ty.
-  Parameter hTLater : hterm ty → hterm ty.
-  Parameter hTAll : hterm ty → (hterm vl → hterm ty) → hterm ty.
-  Parameter hTMu : (hterm vl → hterm ty) → hterm ty.
-  Parameter hTVMem : label → hterm ty → hterm ty.
-  Parameter hTTMem : label → hterm ty → hterm ty → hterm ty.
-  Parameter hTSel : hterm path → label → nat → ty.
-  Parameter hTNat : hterm ty.
-  Parameter hTSing : hterm path → hterm ty.
-End liftSyn.
-
-Module Import syn <: liftSyn.
-
-Definition htv := liftA1 tv.
-Definition htapp := liftA2 tapp.
-Definition htproj t l := Eval cbv in liftA2 tproj t (pureS l).
-Definition htskip := liftA1 tskip.
-
+Definition htv : hvl → hterm tm := liftA1 tv.
+Definition htapp : hterm tm → hterm tm → hterm tm := liftA2 tapp.
+Definition htproj : hterm tm → label → nat → tm := Eval cbv in λ t l, liftA2 tproj t (pureS l).
+Definition htskip : hterm tm → hterm tm := liftA1 tskip.
 
 Definition hvar_vl : var → hterm vl := ids_hvl.
 Goal hvar_vl = λ n i, var_vl (n + i). done. Abort.
 
-Definition hvnat n := liftA1 vnat (pureS n).
+Definition hvnat : nat → hterm vl := λ n, liftA1 vnat (pureS n).
 Goal hvnat = λ n, liftA0 (vnat n). done. Abort.
 
-Definition hvabs := liftBind vabs.
+Definition hvabs : (hterm vl → hterm tm) → hterm vl := liftBind vabs.
 
-Definition hvobj (ds : hterm vl → list (label * hterm dm)) : hterm vl :=
+Definition hvobj : (hterm vl → hdms) → hterm vl := λ ds,
   liftBind vobj (liftList ∘ ds).
 
-Definition hdtysyn := liftA1 dtysyn.
+Definition hdtysyn : hterm ty → hterm dm := liftA1 dtysyn.
 (* Not sure about [hdtysem], and not needed. *)
-Definition hdvl := liftA1 dvl.
+Definition hdvl : hterm vl → hterm dm := liftA1 dvl.
 
-Definition hpv := liftA1 pv.
-Definition hpself p l := Eval cbv in liftA2 pself p (pureS l).
+Definition hpv : hterm vl → hterm path := liftA1 pv.
+Definition hpself : hterm path → label → nat → path := Eval cbv in λ p l, liftA2 pself p (pureS l).
 
-Definition hTTop := liftA0 TTop.
-Definition hTBot := liftA0 TBot.
-Definition hTAnd := liftA2 TAnd.
-Definition hTOr := liftA2 TOr.
-Definition hTLater := liftA1 TLater.
+Definition hTTop : hterm ty := liftA0 TTop.
+Definition hTBot : hterm ty := liftA0 TBot.
+Definition hTAnd : hterm ty → hterm ty → hterm ty := liftA2 TAnd.
+Definition hTOr : hterm ty → hterm ty → hterm ty := liftA2 TOr.
+Definition hTLater : hterm ty → hterm ty := liftA1 TLater.
 
-Definition hTAll (T : hterm ty) (U : hterm vl → hterm ty) : hterm ty := λ i,
+Definition hTAll : hterm ty → (hterm vl → hterm ty) → hterm ty := λ T U i,
   (* liftBind (liftA1 TAll T i) U i. *)
   liftBind (TAll (T i)) U i.
 
@@ -133,16 +114,42 @@ Eval cbv -[plus minus] in hTAll.
 Goal hTAll = λ T U i, (TAll (T i) (U (λ x, var_vl (x - S i)) (S i)))%ty. done. Abort.
 (* Goal hTAll = λ T U i, (∀ (T i), U (λ x, var_vl (x - S i)) (S i))%ty. done. Abort. *)
 
-Definition hTMu := liftBind TMu.
-Definition hTVMem l := liftA1 (TVMem l).
-Definition hTTMem l := liftA2 (TTMem l).
-Definition hTSel p l := Eval cbv in liftA2 TSel p (pureS l).
-Definition hTNat := liftA0 TNat.
-Definition hTSing := liftA1 TSing.
+Definition hTMu : (hterm vl → hterm ty) → hterm ty := liftBind TMu.
+Definition hTVMem : label → hterm ty → hterm ty := λ l, liftA1 (TVMem l).
+Definition hTTMem : label → hterm ty → hterm ty → hterm ty := λ l, liftA2 (TTMem l).
+Definition hTSel : hterm path → label → nat → ty := Eval cbv in λ p l, liftA2 TSel p (pureS l).
+Definition hTNat : hterm ty := liftA0 TNat.
+Definition hTSing : hterm path → hterm ty := liftA1 TSing.
 
 Arguments hvobj _%dms.
 Arguments hTAll _%ty _%ty.
 Arguments hTMu _%ty.
+
+Arguments htv /.
+Arguments htapp /.
+Arguments htproj /.
+Arguments htskip /.
+Arguments hvar_vl /.
+Arguments hvnat /.
+Arguments hvabs /.
+Arguments hvobj /.
+Arguments hdtysyn /.
+Arguments hdvl /.
+Arguments hpv /.
+Arguments hpself /.
+Arguments hTTop /.
+Arguments hTBot /.
+Arguments hTAnd /.
+Arguments hTOr /.
+Arguments hTLater /.
+Arguments hTAll /.
+Arguments hTMu /.
+Arguments hTVMem /.
+Arguments hTTMem /.
+Arguments hTSel /.
+Arguments hTNat /.
+Arguments hTSing /.
+
 End syn.
 
 Module tests1.
@@ -154,6 +161,7 @@ Eval cbv in ex.
 End tests1.
 
 Module Import hoasNotation.
+Export syn.
 (* Notations. *)
 Open Scope dms_scope.
 Notation " {@ } " := (@nil (string * hterm dm)) (format "{@ }") : dms_scope.
@@ -167,7 +175,26 @@ Notation " {@ x ; y ; .. ; z } " :=
 Close Scope dms_scope.
 Arguments hvobj _%dms_scope.
 
+(* Useful for writing functions whose body is in scope [%ty]. *)
+Notation "'λT' x .. y , t" := (fun x => .. (fun y => t%ty) ..)
+  (at level 200, x binder, y binder, right associativity, only parsing,
+  format "'[  ' '[  ' 'λT'  x  ..  y ']' ,  '/' t ']'") : ty_scope.
+
+(* Useful for writing functions whose body is in scope [%dms]. *)
+Notation "'λD' x .. y , t" := (fun x => .. (fun y => t%dms) ..)
+  (at level 200, x binder, y binder, right associativity, only parsing,
+  format "'[  ' '[  ' 'λD'  x  ..  y ']' ,  '/' t ']'") : dms_scope.
+
+Notation "'λ:' x , t" := (hvabs (fun x => t))
+  (at level 200, right associativity,
+  format "'[  ' '[  ' 'λ:'  x  ']' ,  '/' t ']'").
+
+Notation "'λ::' x .. y , t" := (htv (hvabs (fun x => .. (htv (hvabs (fun y => t))) ..)))
+  (at level 200, x binder, y binder, right associativity,
+  format "'[  ' '[  ' 'λ::'  x  ..  y ']' ,  '/' t ']'").
+
 Notation "'ν' ds " := (hvobj ds) (at level 60, ds at next level).
+Notation "'ν:' x , ds " := (hvobj (λD x, ds)) (at level 60, ds at next level).
 Notation "'val' l = v" := (l, hdvl v) (at level 60, l at level 50).
 Notation "'type' l = T  " := (l, hdtysyn T) (at level 60, l at level 50).
 
@@ -188,11 +215,6 @@ Notation "'▶'" := hTLater : ty_scope.
 (* Level taken from Iris. *)
 Notation "'▶' T" := (hTLater T) (at level 49, right associativity) : ty_scope.
 
-(* Useful for writing functions whose body is in scope [%ty]. *)
-Notation "'λT' x .. y , t" := (fun x => .. (fun y => t%ty) ..)
-  (at level 200, x binder, y binder, right associativity,
-  format "'[  ' '[  ' 'λT'  x  ..  y ']' ,  '/' t ']'") : ty_scope.
-
 Notation "'∀:' x : T , U" := (hTAll T (λT x, U)) (at level 48, x, T at level 98, U at level 98).
 Notation "'μ' Ts" := (hTMu Ts) (at level 50, Ts at next level).
 Notation "'μ:' x , Ts" := (hTMu (λT x, Ts)) (at level 50, Ts at next level).
@@ -209,6 +231,7 @@ Notation tparam A := (type A >: ⊥ <: ⊤)%ty.
 Definition typeEq l T := (type l >: T <: T) % ty.
 
 Notation hx := hvar_vl.
+
 Notation hx0 := (hx 0).
 Notation hx1 := (hx 1).
 Notation hx2 := (hx 2).
@@ -217,6 +240,15 @@ Notation hx4 := (hx 4).
 Notation hx5 := (hx 5).
 Notation hx6 := (hx 6).
 
+Notation hpx n := (hpv (hx n)).
+Notation hp0 := (hpx 0).
+Notation hp1 := (hpx 1).
+Notation hp2 := (hpx 2).
+Notation hp3 := (hpx 3).
+Notation hp4 := (hpx 4).
+Notation hp5 := (hpx 5).
+Notation hp6 := (hpx 6).
+
 End hoasNotation.
 
 
@@ -224,6 +256,14 @@ Module tests.
 Eval cbv in hclose {@ hTNat ; hTNat ; hTNat } % ty.
 
 Definition ex := hclose $ hTAll hTNat (λ x, hTMu (λ y, hTAnd (hTSing (hpv x)) (hTSing (hpv y)))).
+
+Definition ex2 := hclose (λ: f, htv f).
+Eval cbv in ex2.
+Goal ex2 = vabs (tv DBNotation.x0). done. Qed.
+Definition ex3 := hclose (λ:: f x, htapp (htv f) (htv x)).
+Eval cbv in ex3.
+Goal ex3 = tv (vabs (tv (vabs (tapp (tv DBNotation.x1) (tv DBNotation.x0))))). done. Qed.
+
 (** * Second, real, test *)
 Definition listTBodyGen bool sci L U : hterm ty := μ λT self, {@
   type "A" >: L <: U;
@@ -236,7 +276,7 @@ Definition consTResConcr bool sci U := listTBodyGen bool sci U U.
 
  (* : ty_scope. *)
 Definition consTConcr bool sci : hterm ty :=
-  ∀: x: tparam "T",
+  ∀: x : tparam "T",
     hpv x @; "T" →:
       hTAnd (hpv sci @; "List") (type "A" >: ⊥ <: hpv x @; "T") →:
       (consTResConcr bool sci (hpv x @; "T")).
