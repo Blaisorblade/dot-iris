3f27d3038c95ab6227745ae84c6c2dd29ebc8c7a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-12-07 23:31:04 +0100

    Golf proof

--- theories/Dot/misc/experiments.v
+++ theories/Dot/misc/experiments.v
@@ -185,11 +185,7 @@ Section Sec.
   Lemma Distr_TLater_And T1 T2 ρ v:
     ⟦ TLater (TAnd T1 T2) ⟧ ρ v ⊣⊢
     ⟦ TAnd (TLater T1) (TLater T2) ⟧ ρ v.
-  Proof.
-    iSplit.
-    iIntros "/= [??]"; eauto.
-    iIntros "/= [? ?]"; eauto.
-  Qed.
+  Proof. iSplit; iIntros "/= [$ $]". Qed.
 
   Lemma selfIntersect Γ T U i j:
     Γ ⊨ T, i <: U, j + i -∗
