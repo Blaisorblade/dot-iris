a9d528545bf6f9fbf42d9c064cec6ba982c619ca
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-12-10 01:46:49 +0100

    Translate example from typingExamplesUnst.v

--- scala-examples/coqTranslation/scalaLibV.scala
+++ scala-examples/coqTranslation/scalaLibV.scala
@@ -5,7 +5,7 @@ import typing_unstamped_derivedV._
 
 type Nat = Int // Hack
 
-type HashableString = { val hashCode : () => Nat }
+type HashableString = { val hashCode_ : () => Nat }
 // Notation HashableString := (μ {@ val "hashCode" : TUnit →: TNat }).
 
 object loopDefV { self =>
