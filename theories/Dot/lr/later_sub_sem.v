c22a4676e7c95c81869f33cd1deed7e3216700d4
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-02-20 18:10:03 +0100

    Whitespace fix

--- theories/Dot/lr/later_sub_sem.v
+++ theories/Dot/lr/later_sub_sem.v
@@ -10,7 +10,7 @@ Set Suggest Proof Using.
 Set Default Proof Using "Type".
 
 (* This is specialized to [vnil] because contexts only contain proper types anyway. *)
-Definition s_ty_sub `{HdlangG: dlangG Σ} (T1 T2 : oltyO Σ 0) := ∀ ρ v, T1 vnil ρ v -∗ T2  vnil ρ v.
+Definition s_ty_sub `{HdlangG: dlangG Σ} (T1 T2 : oltyO Σ 0) := ∀ ρ v, T1 vnil ρ v -∗ T2 vnil ρ v.
 Notation "s⊨T T1 <: T2" := (s_ty_sub T1 T2) (at level 74, T1, T2 at next level).
 
 Definition ty_sub `{HdlangG: dlangG Σ} T1 T2 := s⊨T V⟦ T1 ⟧ <: V⟦ T2 ⟧.
