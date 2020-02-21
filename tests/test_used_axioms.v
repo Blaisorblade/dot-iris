50b5cae437e7cc21e1c8598206ee6306eb0cd681
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-02-21 17:57:01 +0100

    Confirm which axioms we use, for significant theorems

---
+++ tests/test_used_axioms.v
@@ -0,0 +1,11 @@
+From D.Dot Require Import fundamental.
+From D.Dot.examples Require examples fromPDotMutualRecSem.
+
+Print Assumptions type_soundness.
+Print Assumptions path_normalization.
+Print Assumptions examples.examples.vSafe.
+Print Assumptions fromPDotMutualRecSem.pcoreSafe.
+
+From D.Dot.examples Require list.
+
+Print Assumptions list.clListTypNat3.
