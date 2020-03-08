af388c5ce9d90cf5d5ae50f8a154f71c58d13987
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-03-08 20:48:51 +0100

    Define new later-stripping judgment in paper

--- theories/Dot/typing/typing_aux_defs.v
+++ theories/Dot/typing/typing_aux_defs.v
@@ -23,6 +23,9 @@ context. *)
 Reserved Notation "⊢G Γ1 <:* Γ2" (at level 74, Γ1, Γ2 at next level).
 Reserved Notation "⊢T T1 <: T2" (at level 74, T1, T2 at next level).
 
+Reserved Notation "⊢G G1 '>>▷*' G2" (at level 74, G1, G2 at next level).
+Reserved Notation "⊢T T1 '>>▷' T2" (at level 74, T1, T2 at next level).
+
 (** A left inverse of TLater. Sometimes written ⊲. *)
 (* Definition unTLater T : ty := match T with | TLater T' => T' | _ => T end. *)
 Fixpoint unTLater T : ty := match T with
@@ -35,12 +38,28 @@ end.
 Definition unTLater_TLater T: unTLater (TLater T) = T := reflexivity _.
 Global Instance: Cancel (=) unTLater TLater. Proof. exact: unTLater_TLater. Qed.
 
+Inductive ty_strip_syn : ty → ty → Prop :=
+| ty_strip_id_syn T : ⊢T T >>▷ T
+| ty_strip_TLater_add_syn T :
+  ⊢T TLater T >>▷ T
+| ty_strip_cong_TAnd_syn T1 T2 U1 U2 :
+  ⊢T T1 >>▷ U1 → ⊢T T2 >>▷ U2 → ⊢T TAnd T1 T2 >>▷ TAnd U1 U2
+| ty_strip_cong_TOr_syn T1 T2 U1 U2 :
+  ⊢T T1 >>▷ U1 → ⊢T T2 >>▷ U2 → ⊢T TOr T1 T2 >>▷ TOr U1 U2
+| ty_strip_cong_TLater_syn T1 T2 :
+  ⊢T T1 >>▷ T2 →
+  ⊢T TLater T1 >>▷ TLater T2
+where "⊢T T1 '>>▷' T2" := (ty_strip_syn T1 T2).
+Hint Constructors ty_strip_syn : ctx_sub.
+
 (** The actual constructor is [ty_sub_TLater_syn]; the other ones are
 just congruence under [TLater], [TAnd], [TOr].
 We also add transitivity: it is not admissible, and the counterexample is
 [⊢T T <: TLater (TLater T)]. *)
 Inductive ty_sub_syn : ty → ty → Prop :=
 | ty_sub_id_syn T : ⊢T T <: T
+| ty_trans_sub_syn T1 T2 T3 :
+  ⊢T T1 <: T2 → ⊢T T2 <: T3 → ⊢T T1 <: T3
 | ty_sub_TLater_add_syn T1 T2 :
   ⊢T T1 <: T2 → ⊢T T1 <: TLater T2
 | ty_sub_cong_TAnd_syn T1 T2 U1 U2 :
@@ -50,21 +69,29 @@ Inductive ty_sub_syn : ty → ty → Prop :=
 | ty_sub_cong_TLater_syn T1 T2 :
   ⊢T T1 <: T2 →
   ⊢T TLater T1 <: TLater T2
+(* | ty_swap_later_and T1 T2 :
+  ⊢T TLater (TAnd T1 T2) <: TAnd (TLater T1) (TLater T2) *)
+| ty_distr_TAnd_TLater_syn T1 T2 :
+  ⊢T TAnd (TLater T1) (TLater T2) <: TLater (TAnd T1 T2)
+| ty_distr_TOr_TLater_syn T1 T2 :
+  ⊢T TOr (TLater T1) (TLater T2) <: TLater (TOr T1 T2)
 (* Unused: *)
 (* | ty_sub_TLater_unTLater_syn T :
   ⊢T T <: TLater (unTLater T). *)
 where "⊢T T1 <: T2" := (ty_sub_syn T1 T2).
 Hint Constructors ty_sub_syn : ctx_sub.
 
+(** Control transitivity to ensure [eauto] does not diverge. *)
+Remove Hints ty_trans_sub_syn : ctx_sub.
+Hint Extern 5 (⊢T _ <: _) => try_once ty_trans_sub_syn : ctx_sub.
+
 Lemma ty_sub_TLater_syn T : ⊢T T <: TLater T. Proof. auto with ctx_sub. Qed.
+Hint Resolve ty_sub_TLater_syn : ctx_sub.
 
-Lemma ty_trans_sub_syn T1 T2 T3 : ⊢T T1 <: T2 → ⊢T T2 <: T3 → ⊢T T1 <: T3.
-Proof.
-  move: T1 => + + Hsub2.
-  induction Hsub2; inversion 1; subst; auto with ctx_sub.
-Qed.
-
-Hint Resolve ty_sub_TLater_syn ty_trans_sub_syn : ctx_sub.
+Lemma ty_strip_to_sub T1 T2 :
+  ⊢T T1 >>▷ T2 →
+  ⊢T T1 <: TLater T2.
+Proof. induction 1; eauto with ctx_sub. Qed.
 
 Lemma unTLater_ty_sub_syn T : ⊢T unTLater T <: T.
 Proof. induction T; cbn; auto with ctx_sub. Qed.
@@ -77,10 +104,22 @@ Abort. *)
 
 Hint Resolve unTLater_ty_sub_syn : ctx_sub.
 
+Inductive ctx_strip_syn : ctx → ctx → Prop :=
+| ctx_strip_nil_syn : ⊢G [] >>▷* []
+| ctx_strip_cons_syn T1 T2 Γ1 Γ2 :
+  ⊢T T1 >>▷ T2 →
+  ⊢G Γ1 >>▷* Γ2 →
+  ⊢G T1 :: Γ1 >>▷* T2 :: Γ2
+where "⊢G Γ1 >>▷* Γ2" := (ctx_strip_syn Γ1 Γ2).
+Hint Constructors ctx_strip_syn : ctx_sub.
+
+Lemma ctx_strip_id_syn Γ : ⊢G Γ >>▷* Γ.
+Proof. elim: Γ => //=; auto with ctx_sub. Qed.
+
+Hint Resolve ctx_strip_id_syn : ctx_sub.
+
 Inductive ctx_sub_syn : ctx → ctx → Prop :=
 | ctx_sub_nil_syn : ⊢G [] <:* []
-(* | ctx_sub_TLater_unTLater_syn Γ :
-  ⊢G Γ <:* TLater <$> (unTLater <$> Γ) *)
 | ctx_sub_cons_syn T1 T2 Γ1 Γ2 :
   ⊢T T1 <: T2 →
   ⊢G Γ1 <:* Γ2 →
@@ -88,15 +127,20 @@ Inductive ctx_sub_syn : ctx → ctx → Prop :=
 where "⊢G Γ1 <:* Γ2" := (ctx_sub_syn Γ1 Γ2).
 Hint Constructors ctx_sub_syn : ctx_sub.
 
-Lemma ctx_id_syn Γ : ⊢G Γ <:* Γ.
+Lemma ctx_sub_id_syn Γ : ⊢G Γ <:* Γ.
 Proof. induction Γ; auto with ctx_sub. Qed.
 
-Lemma ctx_trans_sub_syn Γ1 Γ2 Γ3 :
+Lemma ctx_sub_trans_sub_syn Γ1 Γ2 Γ3 :
   ⊢G Γ1 <:* Γ2 → ⊢G Γ2 <:* Γ3 → ⊢G Γ1 <:* Γ3.
 Proof.
   move: Γ1 => + + Hsub2.
   induction Hsub2; inversion 1; subst; eauto with ctx_sub.
 Qed.
+
+Lemma ctx_strip_to_sub G1 G2 :
+  ⊢G G1 >>▷* G2 →
+  ⊢G G1 <:* TLater <$> G2.
+Proof. elim=> /=; eauto using ty_strip_to_sub with ctx_sub. Qed.
 
 Lemma fmap_ctx_sub_syn {Γ} (f g : ty -> ty):
   (∀ T, ⊢T f T <: g T) →
@@ -124,7 +168,7 @@ Lemma TLater_cong_ctx_sub_syn Γ1 Γ2 :
   ⊢G TLater <$> Γ1 <:* TLater <$> Γ2.
 Proof. induction 1; cbn; auto with ctx_sub. Qed.
 
-Hint Resolve ctx_id_syn ctx_trans_sub_syn unTLater_ctx_sub_syn
+Hint Resolve ctx_sub_id_syn ctx_sub_trans_sub_syn unTLater_ctx_sub_syn
   ctx_sub_TLater_syn TLater_cong_ctx_sub_syn : ctx_sub.
 
 Ltac ietp_weaken_ctx := auto with ctx_sub.
@@ -134,3 +178,6 @@ Proof. by elim => [|> ?? /= ->]. Qed.
 
 Lemma ctx_sub_len_tlater {Γ Γ'} : ⊢G Γ <:* TLater <$> Γ' → length Γ = length Γ'.
 Proof. intros ->%ctx_sub_len. apply fmap_length. Qed.
+
+Lemma ctx_strip_len Γ Γ' : ⊢G Γ >>▷* Γ' → length Γ = length Γ'.
+Proof. by elim => [|> ?? /= ->]. Qed.
