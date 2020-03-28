21f5a5ca0ed40cf9937996f61991e86e14264d67
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-03-28 20:13:26 +0100

    Move ssreflect successor notations into prelude

--- theories/misc_unused/hoInterps_experiments.v
+++ theories/misc_unused/hoInterps_experiments.v
@@ -15,27 +15,7 @@ Set Default Proof Using "Type".
 Set Implicit Arguments.
 Unset Strict Implicit.
 
-Implicit Types (Σ : gFunctors) .
-(** ssreflect postfix notation for the successor and predecessor functions.
-SSreflect uses "pred" for the generic predicate type, and S as a local bound
-variable.*)
-Notation succn := Datatypes.S.
-Notation predn := Peano.pred.
-
-Notation "n .+1" := (succn n) (at level 2, left associativity,
-  format "n .+1") : nat_scope.
-Notation "n .+2" := n.+1.+1 (at level 2, left associativity,
-  format "n .+2") : nat_scope.
-Notation "n .+3" := n.+2.+1 (at level 2, left associativity,
-  format "n .+3") : nat_scope.
-Notation "n .+4" := n.+2.+2 (at level 2, left associativity,
-  format "n .+4") : nat_scope.
-
-Notation "n .-1" := (predn n) (at level 2, left associativity,
-  format "n .-1") : nat_scope.
-Notation "n .-2" := n.-1.-1 (at level 2, left associativity,
-  format "n .-2") : nat_scope.
-
+Implicit Types (Σ : gFunctors).
 
 Module Type HoSemTypes (Import VS : VlSortsFullSig) (Import LWP : LiftWp VS) (Import L : Lty VS LWP).
 
