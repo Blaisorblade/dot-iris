7449f9862bed83402907115b34512eab8b2ac433
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-04-29 15:08:45 +0200

    Drop dead code

--- theories/pure_program_logic/adequacy.v
+++ theories/pure_program_logic/adequacy.v
@@ -13,21 +13,6 @@ Record adequate `{LangDet Λ} (e1 : expr Λ) (φ : val Λ → Prop) := {
     rtc pure_step e1 e2 → not_stuck e2
 }.
 
-(* Theorem adequate_tp_safe {Λ} (e1 e2 : expr Λ) σ  φ :
-  adequate e1 σ φ →
-  rtc erased_step ([e1], σ) ([e2], σ) →
-  is_Some (to_val e2) ∨ ∃ e3, step ([e2], σ) [] ([e3], σ).
-Proof.
-  intros Had ?.
-  destruct (decide (Forall (λ e, is_Some (to_val e)) t2)) as [|Ht2]; [by left|].
-  apply (not_Forall_Exists _), Exists_exists in Ht2; destruct Ht2 as (e2&?&He2).
-  destruct (adequate_not_stuck NotStuck e1 σ1 φ Had t2 σ2 e2) as [?|(κ&e3&σ3&efs&?)];
-    rewrite ?eq_None_not_Some; auto.
-  { exfalso. eauto. }
-  destruct (elem_of_list_split t2 e2) as (t2'&t2''&->); auto.
-  right; exists κ, (t2' ++ e3 :: t2'' ++ efs), σ3; econstructor; eauto.
-Qed. *)
-
 Section adequacy.
 Context `{Hiris : irisG Λ Σ}.
 
@@ -74,27 +59,6 @@ Lemma wptp_result e1 v2 φ n :
   WP e1 {{ v, ⌜φ v⌝ }} -∗
   ▷^n ⌜φ v2⌝.
 Proof. iIntros (Hs) "He /=". by rewrite (wptp_steps Hs) wp_value_inv'. Qed.
-
-(* Lemma wptp_all_result φ κs' s n e1 t1 κs v2 vs2 σ1 σ2 :
-  nsteps n (e1 :: t1, σ1) κs (of_val <$> v2 :: vs2, σ2) →
-  state_interp σ1 (κs ++ κs') (length t1) -∗
-  WP e1 @ s; ⊤ {{ v,
-    state_interp σ2 κs' (length vs2) -∗
-    ([∗ list] v ∈ vs2, fork_post v) -∗ ⌜φ v ⌝ }} -∗
-  wptp s t1
-  -∗ ▷^(S n) ⌜φ v2 ⌝.
-Proof.
-  iIntros (Hstep) "Hσ He Ht". rewrite laterN_later.
-  iDestruct (wptp_steps with "Hσ He Ht") as "H"; first done.
-  iNext.
-  iDestruct "H" as (e2 t2' ?) "(Hσ & H & Hvs)"; simplify_eq/=.
-  rewrite fmap_length. iDestruct (wp_value_inv' with "H") as "H".
-  iAssert ([∗ list] v ∈ vs2, fork_post v)%I with "[> Hvs]" as "Hm".
-  { clear Hstep. iInduction vs2 as [|v vs] "IH"; csimpl; first by iFrame.
-    iDestruct "Hvs" as "[Hv Hvs]".
-    iDestruct (wp_value_inv' with "Hv") as "$". by iApply "IH". }
-  iNext. iApply ("H" with "Hσ Hm").
-Qed. *)
 
 Lemma wp_safe e Φ :
   WP e {{ Φ }} -∗ ⌜not_stuck e⌝.
