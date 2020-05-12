822a694ee2d2f1b5bb865bb356956352d92a562b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-05-12 17:49:02 +0200

    Drop dead code

--- theories/DSub/syn/syn.v
+++ theories/DSub/syn/syn.v
@@ -309,24 +309,6 @@ End VlSorts.
 
 Instance sort_ty : Sort ty := {}.
 
-(** After instantiating Autosubst, a few binding-related syntactic definitions
-    that need not their own file. *)
-
-(** Here is a manual proof of a lemma, with explanations. *)
-Lemma fv_vabs_inv_manual e n : nclosed_vl (vabs e) n → nclosed e (S n).
-Proof.
-  rewrite /nclosed_vl /nclosed => /= Hfv s1 s2 HsEq.
-
-  (** From Hfv, we only learn that [e.|[up s1] = e.|[up s2]], for arbitrary [s1]
-      and [s2], but substitutions in our thesis [e.|[s1] = e.|[s2]] are not of form [up ?].
-      Hence, we rewrite it using [decomp_s] / [decomp_s_vl] to get a
-      substitution of form [up ?], then rewrite with [e.|[up (stail s1)] =
-      e.|[up (stail s2)]] (got from [Hfv]), and conclude.
-      *)
-  rewrite ?(decomp_s _ s1) ?(decomp_s _ s2) ?(decomp_s_vl _ s1) ?(decomp_s_vl _ s2) (eq_n_s_heads HsEq); last lia.
-  injection (Hfv _ _ (eq_n_s_tails HsEq)); rewritePremises; reflexivity.
-Qed.
-
 (** Induction principles for syntax. *)
 
 (* Using a Section is a trick taken from CPDT, but there bodies are hand-written.
