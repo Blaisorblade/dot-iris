d4b268a081d4afb02774a620dae43b1d92e723e3
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-05-22 15:36:59 +0200

    Refix #232: Drop DSub and HKDot from artifact

--- tests/test_strings2.v
+++ tests/test_strings2.v
@@ -2,7 +2,3 @@ From D.Dot Require Import old_fundamental.
 Import prelude.
 
 Goal length [1] = 1. idtac "Dot.old_fundamental". Show. Abort.
-
-From D.DSub Require Import ds_fundamental.
-
-Goal length [1] = 1. idtac "DSub.ds_fundamental". Show. Abort.
