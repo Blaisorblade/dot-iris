ca18e7400362d87cc3b22d041fc7bbcd9ccee8aa
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-05-22 22:10:03 +0200

    Remove unused scripts

--- mkdoc-proj.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-## Produce documentation from the first N files from _CoqProject, where N is
-## passed on the command line.
-#
-nlines="$1"
-./mkdoc.sh $(cat _CoqProject|egrep -v '^($|-|#)'|head -${nlines})
