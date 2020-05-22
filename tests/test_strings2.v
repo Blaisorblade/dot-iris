44ac4ef9462aa785a24045b801bea7a1047ccaab
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-05-22 15:36:59 +0200

    Fix #232: Drop DSub from artifact

--- tests/test_strings2.v
+++ tests/test_strings2.v
@@ -2,7 +2,3 @@ From D.Dot Require Import fundamental.
 Import prelude.
 
 Goal length [1] = 1. idtac "Dot.fundamental". Show. Abort.
-
-From D.DSubSyn Require Import fundamental.
-
-Goal length [1] = 1. idtac "DSubSyn.fundamental". Show. Abort.
