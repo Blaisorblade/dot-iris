59ed9c20b1e0fee8f7e0a70854b9c712c0878340
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-06-05 02:28:09 +0200

    codesize.sh: revise to make certain points

--- codesize.sh
+++ codesize.sh
@@ -57,11 +57,15 @@ sumDirs "Unused" "misc_unused Dot/misc_unused DSub/misc_unused"
 
 prelimDirs=". iris_extra pure_program_logic"
 prelimLoc=$(sumDirsRaw "$prelimDirs")
-dotDirs="$(echo $(find Dot -type d \( -name misc_unused -prune -o -print \)))"
+dotExamplesDirs="$(echo $(find Dot/examples -type d))"
+dotExamplesLoc=$(sumDirsRaw "$dotExamplesDirs")
+dotDirs="$(echo $(find Dot -type d \( \( -name misc_unused -o -name examples -o -name hkdot \) -prune -o -print \)))"
 dotLoc=$(sumDirsRaw "$dotDirs")
 
 echo
 format "Preliminaries + DOT" "$prelimDirs $dotDirs" "$[$prelimLoc + $dotLoc]"
+format "Examples" "$dotExamplesDirs" "$dotExamplesLoc"
+format "Preliminaries + DOT + examples" "$prelimDirs $dotDirs $dotExamplesDirs" "$[$prelimLoc + $dotLoc + $dotExamplesLoc]"
 
 echo
 format "Preliminaries" ". iris_extra pure_program_logic" $prelimLoc
@@ -79,11 +83,11 @@ echo
 sumDirs "syntactic typing (w/ fundamental)" ". typing"
 
 echo
-exampleDirs="$(echo $(find examples -type d))"
-sumDirs "examples" "$exampleDirs"
+sumDirs "hkdot" "hkdot"
 
 echo
-sumDirs "hkdot" "hkdot"
+cd ..
+sumDirs "examples" "$dotExamplesDirs"
 # cd DSub
 # echo "DSub"
 # sumDirs "DSub syntax" "syn"
