8a359a1d4196a00c24af9393b244ce2c341d4b68
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-06-17 18:48:46 +0200

    Rewrite, to allow reuse in swap_later_impl.v

--- theories/iris_extra/persistence.v
+++ theories/iris_extra/persistence.v
@@ -1,68 +1,71 @@
-From stdpp Require Import proof_irrel.
+(** * Show all of Iris propositions are persistent when resources are idempotent (like in our case). *)
 From iris.algebra Require Import agree gmap.
-From iris.base_logic Require Import saved_prop.
+From iris.base_logic Require Import lib.iprop.
 
-Class PersistentFunct (F : rFunctor) := persistent_func :
-    ∀ A `{!Cofe A} B `{!Cofe B} (x : rFunctor_car F A B), CoreId x.
+From D Require Import cmra_prop_lift.
 
-Class FullyPersistent (Σ : gFunctors) := fully_persistent :
-    ∀ (i : gid Σ), PersistentFunct (gFunctors_lookup Σ i).
+Set Suggest Proof Using.
+Set Default Proof Using "Type".
 
-Instance fully_persistent_core_id `{fp : FullyPersistent Σ} (x : iResUR Σ) :
-  CoreId x.
+Class CmraPersistent (M : cmraT) :=
+  cmra_persistent (x : M) :> CoreId x.
+
+Section CmraPersistentLaws.
+  Local Arguments uPred_holds {_} !_ _ _.
+
+  Global Instance CmraPersistent_Persistent {M : ucmraT} `(!CmraPersistent M)
+    (P : uPred M) : Persistent P.
+  Proof.
+    split => n x Hx Hpx; uPred.unseal.
+    rewrite core_id_core. done.
+  Qed.
+End CmraPersistentLaws.
+
+(** *** Lift [cmra_prop_lift] infrastructure to [CmraPersistent]. *)
+Notation PersistentRFunct F := (LiftCPropToRFunctor CmraPersistent F).
+Notation PersistentGFunct Σ := (LiftCPropToGFunctor CmraPersistent Σ).
+
+Instance PersistentGFunct_nil : LiftCPropToGFunctor_nil_type CmraPersistent.
+Proof. apply LiftCPropToGFunctor_nil. Qed.
+
+Instance PersistentGFunct_app : LiftCPropToGFunctor_app_type CmraPersistent.
+Proof. apply LiftCPropToGFunctor_app. Qed.
+
+Instance PersistentGFunct_GFunctor `{!rFunctorContractive F} :
+  LiftCPropToGFunctor_GFunctor_type F CmraPersistent.
+Proof. apply LiftCPropToGFunctor_GFunctor. Qed.
+
+(** *** Show that [iResUR] and [iProp] lift [CmraPersistent], using [cmra_prop_lift]. *)
+Instance CmraPersistent_discrete_funUR {A} (B : A → ucmraT)
+  `(∀ i, CmraPersistent (B i)) :
+  CmraPersistent (discrete_funUR B).
 Proof.
-  apply Some_proper; intros i j.
+  move=> f; unfold CoreId.
+  apply Some_proper => a.
+  apply: core_id_core.
+Qed.
+
+Instance CmraPersistent_gmapUR `{Countable A} `(!CmraPersistent T):
+  CmraPersistent (gmapUR A T).
+Proof.
+  intros j.
+  apply Some_proper => k.
   rewrite lookup_core.
-  destruct (x i !! j) as [y|] eqn:Heq; rewrite Heq; last done.
-  rewrite /core /=.
-  apply fp.
+  case: (j !! k) => [y|//].
+  apply: core_id.
 Qed.
 
-Local Arguments uPred_holds !_ _ _.
+Instance CmraPersistent_iResUR `(fp : PersistentGFunct Σ) :
+  CmraPersistent (iResUR Σ) := lift_cprop_iResUR.
 
-Instance FullyPersistent_Persistent `{!FullyPersistent Σ} (P : iProp Σ) :
-  Persistent P.
+(** * Show that our actual resources satisfy [CmraPersistent]. *)
+Instance CmraPersistent_agreeR F : CmraPersistent (agreeR F).
+Proof. intros ?; apply _. Qed.
+
+(* Currently unused instances. *)
+Instance CmraPersistent_optionUR `{!CmraPersistent A}: CmraPersistent (optionUR A).
 Proof.
-  split; intros n x Hx Hpx.
-  uPred.unseal.
-  rewrite /uPred_persistently_def /=.
-  rewrite core_id_core; done.
+  intros x.
+  apply Some_proper.
+  apply: core_id_core.
 Qed.
-
-Instance FullyPersistent_nil : FullyPersistent gFunctors.nil.
-Proof. intros i; inversion i. Qed.
-
-Instance FullyPersistent_app `{!FullyPersistent Σ, !FullyPersistent Σ'} :
-  FullyPersistent (gFunctors.app Σ Σ').
-Proof.
-  intros i.
-  apply fin_plus_inv with (i := i); intros;
-    [rewrite /= fin_plus_inv_L|rewrite /= fin_plus_inv_R]; auto.
-Qed.
-
-Instance FullyPersistent_GFunctor `{!rFunctorContractive F}
-         `{fp : !PersistentFunct F} :
-  FullyPersistent #[ GFunctor F].
-Proof.
-  intros ? ? ? ? ?.
-  assert (gFunctors_lookup #[GFunctor F] i = GFunctor F) as ->; last done.
-  refine (match i as z in fin u return
-                fin_to_nat z < 1  →
-                ∀ Heq : u = 1,
-                gFunctors_lookup #[ GFunctor F]
-                                 match Heq in _ = l return fin l with
-                                   eq_refl => z
-                                 end = GFunctor F with
-            @Fin.F1 k => λ Hlt Heq, _ | Fin.FS _ => _
-          end (fin_to_nat_lt i) eq_refl); last by simpl; lia.
-  destruct k; last lia.
-  rewrite (eq_pi _ _ Heq eq_refl) /=; done.
-Qed.
-
-Instance PersistentFunct_agree F : PersistentFunct (agreeRF F).
-Proof. intros ? ? ? ? ?; apply _. Qed.
-
-(* testing *)
-Local Instance PersistentFunct_savedAnythingΣ `{!oFunctorContractive F} :
-  FullyPersistent (savedAnythingΣ F).
-Proof. apply _. Qed.
