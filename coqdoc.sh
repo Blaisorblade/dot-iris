87ad39e8d9a1633aa6d38188ac351b6b44ca0706
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-06-18 13:39:43 +0200

    coqdoc.sh: Avoid extra slashes in external links

--- coqdoc.sh
+++ coqdoc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 EXTRA_DIR=coqdocjs/extra
 COQDOCFLAGS="\
-  --external https://plv.mpi-sws.org/coqdoc/stdpp/ stdpp \
-  --external https://plv.mpi-sws.org/coqdoc/iris/ iris \
-  --external https://www.ps.uni-saarland.de/autosubst/doc/ Autosubst \
+  --external https://plv.mpi-sws.org/coqdoc/stdpp stdpp \
+  --external https://plv.mpi-sws.org/coqdoc/iris iris \
+  --external https://www.ps.uni-saarland.de/autosubst/doc Autosubst \
   --toc --toc-depth 2 --html --interpolate \
   --index indexpage --no-lib-name --parse-comments --utf8 \
   --with-header ${EXTRA_DIR}/header.html --with-footer ${EXTRA_DIR}/footer.html"
