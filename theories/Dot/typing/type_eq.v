6af15c80c4c6f836ff960510a612746dd8504f41
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-06-24 15:58:17 +0200

    More robust proof hints

--- theories/Dot/typing/type_eq.v
+++ theories/Dot/typing/type_eq.v
@@ -34,27 +34,26 @@ Inductive type_equiv : Equiv ty :=
 where "|- T1 == T2" := (type_equiv T1 T2).
 
 Existing Instance type_equiv.
+Local Hint Constructors type_equiv : core.
+Remove Hints type_equiv_sym type_equiv_trans : core.
 
 Instance type_equiv_refl: Reflexive type_equiv.
-Proof. intros T; induction T; eauto using type_equiv. Qed.
+Proof. intros T; induction T; auto. Qed.
 
-Existing Instance type_equiv_sym.
-Existing Instance type_equiv_trans.
-
-Local Hint Constructors type_equiv : core.
+Existing Instances type_equiv_sym type_equiv_trans.
 
 Lemma type_equiv_laterN_and j U1 U2 :
   |- iterate TLater j (TAnd U1 U2)
   == TAnd (iterate TLater j U1) (iterate TLater j U2).
-Proof. elim: j U1 U2 => [//|j IHj] U1 U2; rewrite !iterate_S; eauto. Qed.
+Proof. elim: j U1 U2 => [//|j IHj] U1 U2; rewrite !iterate_S; eauto 3 using type_equiv_trans. Qed.
   (* etrans; last apply type_equiv_later_and. by constructor. *)
 
 Lemma type_equiv_laterN_or j U1 U2 :
   |- iterate TLater j (TOr U1 U2)
   == TOr (iterate TLater j U1) (iterate TLater j U2).
-Proof. elim: j U1 U2 => [//|j IHj] U1 U2; rewrite !iterate_S; eauto. Qed.
+Proof. elim: j U1 U2 => [//|j IHj] U1 U2; rewrite !iterate_S; eauto 3 using type_equiv_trans. Qed.
 
 Lemma type_equiv_laterN_mu j U1 :
   |- iterate TLater j (TMu U1)
   == TMu (iterate TLater j U1).
-Proof. elim: j U1 => [//|j IHj] U1; rewrite !iterate_S; eauto. Qed.
+Proof. elim: j U1 => [//|j IHj] U1; rewrite !iterate_S; eauto 3 using type_equiv_trans. Qed.
