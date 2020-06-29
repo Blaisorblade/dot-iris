c57012f228aa851bd6ca647aa110822bdbf018ab
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2020-06-29 02:25:22 +0200

    Comments

--- theories/Dot/lr/dlang_inst.v
+++ theories/Dot/lr/dlang_inst.v
@@ -1,3 +1,4 @@
+(** * Instantiate Iris and [dlang] with our gDOT language. *)
 From D Require Import iris_prelude dlang.
 From D.Dot Require Export syn.
 (* For local definitions. *)
@@ -6,10 +7,23 @@ From D.pure_program_logic Require Import weakestpre lifting.
 
 Include LiftWp VlSorts.
 
+(**
+Some tactics/proof automation for gDOT.
+
+_Names_ are inspired by the names of Iris's HeapLang tactics, and the
+behaviors are vaguely similar, but the implementations are more naive/less
+efficient.
+*)
+
+(** Reduce pure computation steps. *)
 Ltac wp_pure := rewrite -wp_pure_step_later -1?wp_value; last done; iNext.
 
+(** Apply the [wp_bind] rule. *)
 Tactic Notation "wp_bind" uconstr(p) :=
   iApply (wp_bind (ectx_language.fill [p])).
+
+(** Apply a WP using the [wp_wand] rule; automatic application of [wp_bind],
+as in Iris's [wp_apply], is not implemented. *)
 Ltac wp_wapply spec_pat := iApply (wp_wand with spec_pat).
 
 Tactic Notation "smart_wp_bind" uconstr(ctx) ident(v) constr(Hv) uconstr(Hp) :=
