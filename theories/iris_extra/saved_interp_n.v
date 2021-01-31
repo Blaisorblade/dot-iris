573ac563805dd835253198d29f913f7ed1e89302
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-01-31 17:22:19 +0100

    saved_pred3 -> saved_ho_sem_type

--- theories/iris_extra/saved_interp_n.v
+++ theories/iris_extra/saved_interp_n.v
@@ -41,36 +41,36 @@ Definition hoD Σ := astream -d> vl -d> iPropO Σ.
 Notation savedHoEnvPredG s Σ := (savedPredG Σ (argType s)).
 Notation savedHoEnvPredΣ s := (savedPredΣ (argType s)).
 
-Section saved_pred3.
+Section saved_ho_sem_type.
   Context {s : Type}.
   Context `{!savedHoEnvPredG s Σ}.
   Implicit Type (Φ : astream -d> env -d> s -d> iPropO Σ).
 
   Definition curryC Φ : argType s -d> iPropO Σ := (λ '(args, ρ, c), Φ args ρ c).
 
-  Definition saved_pred3_own (γ : gname) Φ :=
+  Definition saved_ho_sem_type_own (γ : gname) Φ :=
     saved_pred_own γ (curryC Φ).
 
-  #[global] Instance saved_pred3_own_contractive γ : Contractive (saved_pred3_own γ).
+  #[global] Instance saved_ho_sem_type_own_contractive γ : Contractive (saved_ho_sem_type_own γ).
   Proof.
-    rewrite /saved_pred3_own => n f g Hfg.
+    rewrite /saved_ho_sem_type_own => n f g Hfg.
     apply saved_pred_own_contractive. case: n Hfg => [//|n Hfg [[]] //].
   Qed.
 
-  #[global] Instance persistent_saved_pred3_own γ Φ : Persistent (saved_pred3_own γ Φ) := _.
+  #[global] Instance persistent_saved_ho_sem_type_own γ Φ : Persistent (saved_ho_sem_type_own γ Φ) := _.
 
-  Lemma saved_pred3_alloc Φ : ⊢ |==> ∃ γ, saved_pred3_own γ Φ.
+  Lemma saved_ho_sem_type_alloc Φ : ⊢ |==> ∃ γ, saved_ho_sem_type_own γ Φ.
   Proof. apply saved_pred_alloc. Qed.
 
-  Lemma saved_pred3_agree {γ Φ Ψ} args ρ c :
-    saved_pred3_own γ Φ -∗ saved_pred3_own γ Ψ -∗
+  Lemma saved_ho_sem_type_agree {γ Φ Ψ} args ρ c :
+    saved_ho_sem_type_own γ Φ -∗ saved_ho_sem_type_own γ Ψ -∗
     ▷ (Φ args ρ c ≡ Ψ args ρ c).
   Proof. apply (saved_pred_agree γ (curryC Φ) (curryC Ψ) (_, _, _)). Qed.
-End saved_pred3.
+End saved_ho_sem_type.
 
-Typeclasses Opaque saved_pred3_own.
-#[global] Opaque saved_pred3_own.
+Typeclasses Opaque saved_ho_sem_type_own.
+#[global] Opaque saved_ho_sem_type_own.
 
-Notation "γ ⤇ φ" := (saved_pred3_own γ φ) (at level 20).
+Notation "γ ⤇ φ" := (saved_ho_sem_type_own γ φ) (at level 20).
 
 End SavedHoInterp.
